a90bed4245e46ebd59a7652d18db2a5dc04699a2
Matthew Jensen <mjensen@atlassian.com>
2011-06-20 20:31:39 +1000

    Added active objects support

---
+++ src/main/java/com/atlassian/jira/plugins/bitbucket/ao/Repository.java
@@ -0,0 +1,10 @@
+package com.atlassian.jira.plugins.bitbucket.ao;
+
+import net.java.ao.Entity;
+
+/**
+ * Persisted information about a linked repository
+ */
+public interface Repository extends Entity
+{
+}
