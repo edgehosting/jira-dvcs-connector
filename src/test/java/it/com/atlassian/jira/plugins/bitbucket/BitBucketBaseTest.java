ca75585a3814aaa7db08abf0902ee6ce393fa39b
farmas <farmas@atlassian.com>
2011-06-23 18:13:19 +1000

    Adding hamcrest to increase legibility of tests.

--- src/test/java/it/com/atlassian/jira/plugins/bitbucket/BitBucketBaseTest.java
+++ src/test/java/it/com/atlassian/jira/plugins/bitbucket/BitBucketBaseTest.java
@@ -1,10 +1,18 @@
 package it.com.atlassian.jira.plugins.bitbucket;
 
+import com.atlassian.jira.plugins.bitbucket.pageobjects.component.BitBucketCommitEntry;
 import com.atlassian.jira.plugins.bitbucket.pageobjects.page.BitBucketConfigureRepositoriesPage;
+import com.atlassian.jira.plugins.bitbucket.pageobjects.page.JiraViewIssuePage;
 import com.atlassian.pageobjects.TestedProductFactory;
 import com.atlassian.pageobjects.page.HomePage;
 import com.atlassian.webdriver.jira.JiraTestedProduct;
 import org.junit.Before;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * Base class for BitBucket integration tests. Initializes the JiraTestedProduct and logs admin in.
@@ -32,4 +40,21 @@ public abstract class BitBucketBaseTest
             configureRepos = jira.gotoLoginPage().loginAsSysAdmin(BitBucketConfigureRepositoriesPage.class);
         }
     }
+
+    protected void ensureRepositoryPresent(String projectKey, String repoUrl)
+    {
+        if(configureRepos.isRepositoryPresent(projectKey, repoUrl + "/default") == false)
+        {
+            configureRepos.addPublicRepoToProject(projectKey, repoUrl);
+        }
+    }
+
+   
+    protected List<BitBucketCommitEntry> getCommitsForIssue(String issueKey)
+    {
+        return jira.visit(JiraViewIssuePage.class, issueKey)
+                              .openBitBucketPanel()
+                              .waitForMessages();
+    }
+
 }
