35a65ac31c0c3196c5effd64b36c48e14f551528
dusanhornik <devnull@localhost>
2011-11-02 11:12:19 +0100

    BBC-104 adding users with Permissions.ADMINISTER into canAdmin()

--- src/main/java/com/atlassian/jira/plugins/bitbucket/rest/RootResource.java
+++ src/main/java/com/atlassian/jira/plugins/bitbucket/rest/RootResource.java
@@ -35,47 +35,53 @@ import com.google.common.collect.Lists;
 @Path("/")
 public class RootResource
 {
-	
-    @Context UriInfo uriInfo;
+
+    @Context
+    UriInfo uriInfo;
 
     private final PermissionManager permissionManager;
     private final ProjectManager projectManager;
     private final JiraAuthenticationContext jiraAuthenticationContext;
-	private final RepositoryManager globalRepositoryManager;
-	private final Synchronizer synchronizer;
+    private final RepositoryManager globalRepositoryManager;
+    private final Synchronizer synchronizer;
 
-	private Function<SourceControlRepository, Repository> TO_REST_REPOSITORY = new Function<SourceControlRepository, Repository>()
-	{
-		public Repository apply(SourceControlRepository from)
-		{
-			Repository repo = new Repository(from.getId(), from.getProjectKey(), from.getUrl(), from.getUsername(), null, from.getAdminUsername(), null); // don't include the password
-			Progress progress = synchronizer.getProgress(from);
-			if (progress!=null)
-				repo.setStatus(new SyncProgress(progress.isFinished(), progress.getChangesetCount(), progress.getJiraCount(), progress.getError()));
-			return repo;
-		}
-	};
+    private final Function<SourceControlRepository, Repository> TO_REST_REPOSITORY = new Function<SourceControlRepository, Repository>()
+    {
+        public Repository apply(SourceControlRepository from)
+        {
+            Repository repo = new Repository(from.getId(), from.getProjectKey(), from.getUrl(), from.getUsername(),
+                    null, from.getAdminUsername(), null); // don't include the
+                                                          // password
+            Progress progress = synchronizer.getProgress(from);
+            if (progress != null)
+                repo.setStatus(new SyncProgress(progress.isFinished(), progress.getChangesetCount(), progress
+                        .getJiraCount(), progress.getError()));
+            return repo;
+        }
+    };
 
-	public RootResource(@Qualifier("globalRepositoryManager") RepositoryManager globalRepositoryManager, 
-    					PermissionManager permissionManager, ProjectManager projectManager, 
-    					JiraAuthenticationContext jiraAuthenticationContext, Synchronizer synchronizer)
+    public RootResource(@Qualifier("globalRepositoryManager") RepositoryManager globalRepositoryManager,
+            PermissionManager permissionManager, ProjectManager projectManager,
+            JiraAuthenticationContext jiraAuthenticationContext, Synchronizer synchronizer)
     {
         this.globalRepositoryManager = globalRepositoryManager;
-		this.permissionManager = permissionManager;
+        this.permissionManager = permissionManager;
         this.projectManager = projectManager;
         this.jiraAuthenticationContext = jiraAuthenticationContext;
-		this.synchronizer = synchronizer;
+        this.synchronizer = synchronizer;
     }
 
     private boolean canAdmin(String projectKey)
     {
         Project project = projectManager.getProjectObjByKey(projectKey);
-        return project != null &&
-                permissionManager.hasPermission(Permissions.PROJECT_ADMIN, project, jiraAuthenticationContext.getLoggedInUser());
+        return project != null
+                && (permissionManager.hasPermission(Permissions.PROJECT_ADMIN, project,
+                        jiraAuthenticationContext.getLoggedInUser()) || permissionManager.hasPermission(
+                        Permissions.ADMINISTER, jiraAuthenticationContext.getLoggedInUser()));
     }
 
     @GET
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
     @Path("/repositories/")
     public Response getAllRepositories()
     {
@@ -85,15 +91,15 @@ public class RootResource
         {
             if (canAdmin(project.getKey()))
             {
-            	List<SourceControlRepository> repositories = globalRepositoryManager.getRepositories(project.getKey());
-            	list.addAll(Lists.transform(repositories, TO_REST_REPOSITORY));
+                List<SourceControlRepository> repositories = globalRepositoryManager.getRepositories(project.getKey());
+                list.addAll(Lists.transform(repositories, TO_REST_REPOSITORY));
             }
         }
         return Response.ok(new RepositoryList(list)).build();
     }
 
     @GET
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
     @Path("/repository/{id}")
     public Response getRepository(@PathParam("id") int id)
     {
@@ -105,65 +111,65 @@ public class RootResource
     }
 
     @GET
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
     @Path("/repositories/{projectKey}")
     public Response getRepositories(@PathParam("projectKey") String projectKey)
     {
         if (canAdmin(projectKey))
         {
             List<Repository> list = new ArrayList<Repository>();
-        	List<SourceControlRepository> repositories = globalRepositoryManager.getRepositories(projectKey);
-        	list.addAll(Lists.transform(repositories, TO_REST_REPOSITORY));
+            List<SourceControlRepository> repositories = globalRepositoryManager.getRepositories(projectKey);
+            list.addAll(Lists.transform(repositories, TO_REST_REPOSITORY));
 
-        	return Response.ok(new RepositoryList(list)).build();
-        }
-        else
+            return Response.ok(new RepositoryList(list)).build();
+        } else
             return Response.status(Response.Status.FORBIDDEN).build();
     }
 
     @POST
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
     @Path("/repository/{id}/sync")
     public Response startRepositorySync(@PathParam("id") int id, @QueryParam("payload") String payload)
     {
-    	SourceControlRepository repository = globalRepositoryManager.getRepository(id);
-    	if (payload == null)
-    	{
-    		synchronizer.synchronize(repository);
-    	} else
-    	{
-    		List<Changeset> changesets = globalRepositoryManager.parsePayload(repository, payload);
-    		synchronizer.synchronize(repository, changesets);
-    	}
-		// redirect to Repository resource - that will contain sync message/status
-		UriBuilder ub = uriInfo.getBaseUriBuilder();
-		URI uri = ub.path("/repository/{id}").build(id);
-		return Response.seeOther(uri).build();    	
+        SourceControlRepository repository = globalRepositoryManager.getRepository(id);
+        if (payload == null)
+        {
+            synchronizer.synchronize(repository);
+        } else
+        {
+            List<Changeset> changesets = globalRepositoryManager.parsePayload(repository, payload);
+            synchronizer.synchronize(repository, changesets);
+        }
+        // redirect to Repository resource - that will contain sync
+        // message/status
+        UriBuilder ub = uriInfo.getBaseUriBuilder();
+        URI uri = ub.path("/repository/{id}").build(id);
+        return Response.seeOther(uri).build();
     }
 
     @POST
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
     @Path("/repository")
     public Response addRepository(Repository repository)
     {
         if (canAdmin(repository.getProjectKey()))
         {
-        	String url = repository.getUrl();
-        	String projectKey = repository.getProjectKey();
-        	String username = repository.getUsername();
-        	String password = repository.getPassword();
-        	String adminUsername = repository.getUsername();
-        	String adminPassword = repository.getPassword();
-        	
-        	SourceControlRepository repo = globalRepositoryManager.addRepository(projectKey, url, username, password, adminUsername, adminPassword);
+            String url = repository.getUrl();
+            String projectKey = repository.getProjectKey();
+            String username = repository.getUsername();
+            String password = repository.getPassword();
+            String adminUsername = repository.getUsername();
+            String adminPassword = repository.getPassword();
+
+            SourceControlRepository repo = globalRepositoryManager.addRepository(projectKey, url, username, password,
+                    adminUsername, adminPassword);
             return Response.ok(TO_REST_REPOSITORY.apply(repo)).build();
-        }
-        else
+        } else
             return Response.status(Response.Status.FORBIDDEN).build();
     }
 
     @DELETE
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
     @Path("/repository/{id}")
     public Response removeRepository(@PathParam("id") int id)
     {
@@ -172,8 +178,7 @@ public class RootResource
         {
             globalRepositoryManager.removeRepository(id);
             return Response.ok().build();
-        }
-        else
+        } else
             return Response.status(Response.Status.FORBIDDEN).build();
     }
 
