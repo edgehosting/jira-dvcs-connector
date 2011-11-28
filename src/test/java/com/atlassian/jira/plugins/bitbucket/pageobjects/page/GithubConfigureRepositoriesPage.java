ae426b01fdb56ecbcb06747d3bfedecba0e89fb6
dusanhornik <devnull@localhost>
2011-11-28 15:00:30 +0100

    Cleaning up warnings

--- src/test/java/com/atlassian/jira/plugins/bitbucket/pageobjects/page/GithubConfigureRepositoriesPage.java
+++ src/test/java/com/atlassian/jira/plugins/bitbucket/pageobjects/page/GithubConfigureRepositoriesPage.java
@@ -1,13 +1,13 @@
 package com.atlassian.jira.plugins.bitbucket.pageobjects.page;
 
-import com.atlassian.pageobjects.elements.ElementBy;
-import com.atlassian.pageobjects.elements.Options;
-import com.atlassian.pageobjects.elements.PageElement;
-import com.atlassian.pageobjects.elements.WebDriverElement;
-import com.atlassian.pageobjects.elements.query.Poller;
 import org.hamcrest.core.AnyOf;
 import org.hamcrest.core.IsEqual;
 import org.openqa.selenium.By;
+
+import com.atlassian.pageobjects.elements.ElementBy;
+import com.atlassian.pageobjects.elements.Options;
+import com.atlassian.pageobjects.elements.PageElement;
+import com.atlassian.pageobjects.elements.query.Poller;
 
 /**
  * Represents the page to link repositories to projects
@@ -23,7 +23,6 @@ public class GithubConfigureRepositoriesPage extends BaseConfigureRepositoriesPa
     @ElementBy(id = "gh_messages")
     PageElement ghMessagesDiv;
 
-
     /**
      * Links a public repository to the given JIRA project
      *
@@ -33,13 +32,15 @@ public class GithubConfigureRepositoriesPage extends BaseConfigureRepositoriesPa
      * @param adminPassword password used to install the service (postcommit hook)
      * @return BitBucketConfigureRepositoriesPage
      */
-    public String addPublicRepoToProjectAndInstallService(String projectKey, String url, String adminUsername,
-                                                          String adminPassword)
+    @Override
+    @SuppressWarnings("unchecked")
+    public String addPublicRepoToProjectAndInstallService(String projectKey, String url, String adminUsername, String adminPassword)
     {
         urlTextbox.clear().type(url);
         projectSelect.select(Options.value(projectKey));
         addRepositoryButton.click();
-        Poller.waitUntil(addedRepositoryH2.timed().getText(), AnyOf.anyOf(new IsEqual<String>("New Bitbucket repository"), new IsEqual<String>("New Github repository")));
+        Poller.waitUntil(addedRepositoryH2.timed().getText(), AnyOf.anyOf(new IsEqual<String>("New Bitbucket repository"),
+            new IsEqual<String>("New Github repository")));
         // postcommit hook
         addPostCommitServiceCheckbox.click();
         adminUsernameTextbox.clear().type(adminUsername);
@@ -48,7 +49,8 @@ public class GithubConfigureRepositoriesPage extends BaseConfigureRepositoriesPa
         addRepositoryButton.click();
 
         Poller.waitUntilTrue("Expected sync status message to appear.", syncStatusDiv.timed().isVisible());
-        Poller.waitUntilTrue("Expected sync status message to be 'Sync Finished'", syncStatusDiv.find(By.tagName("strong")).timed().hasText("Sync Finished:"));
+        Poller.waitUntilTrue("Expected sync status message to be 'Sync Finished'", syncStatusDiv.find(By.tagName("strong")).timed()
+            .hasText("Sync Finished:"));
 
         return addedRepositoryIdSpan.timed().getValue().byDefaultTimeout();
     }
@@ -60,6 +62,7 @@ public class GithubConfigureRepositoriesPage extends BaseConfigureRepositoriesPa
      * @param url        The url to the bitucket public repo
      * @return BitBucketConfigureRepositoriesPage
      */
+    @Override
     public BaseConfigureRepositoriesPage addPublicRepoToProjectSuccessfully(String projectKey, String url)
     {
         projectSelect.select(Options.value(projectKey));
@@ -67,7 +70,8 @@ public class GithubConfigureRepositoriesPage extends BaseConfigureRepositoriesPa
         addRepositoryButton.click();
 
         Poller.waitUntilTrue("Expected sync status message to appear.", syncStatusDiv.timed().isVisible());
-        Poller.waitUntilTrue("Expected sync status message to be 'Sync Finished'", syncStatusDiv.find(By.tagName("strong")).timed().hasText("Sync Finished:"));
+        Poller.waitUntilTrue("Expected sync status message to be 'Sync Finished'", syncStatusDiv.find(By.tagName("strong")).timed()
+            .hasText("Sync Finished:"));
 
         return this;
     }
@@ -79,12 +83,14 @@ public class GithubConfigureRepositoriesPage extends BaseConfigureRepositoriesPa
      * @param url        The url to the bitucket public repo
      * @return BitBucketConfigureRepositoriesPage
      */
+    @Override
     public GithubConfigureRepositoriesPage addRepoToProjectFailing(String projectKey, String url)
     {
         projectSelect.select(Options.value(projectKey));
         urlTextbox.clear().type(url);
         addRepositoryButton.click();
-        Poller.waitUntilTrue("Expected Error message while connecting repository", messageBarDiv.find(By.tagName("strong")).timed().hasText("Error!"));
+        Poller.waitUntilTrue("Expected Error message while connecting repository", messageBarDiv.find(By.tagName("strong")).timed()
+            .hasText("Error!"));
         return this;
     }
 
@@ -95,26 +101,34 @@ public class GithubConfigureRepositoriesPage extends BaseConfigureRepositoriesPa
      * @param url        The url to the bitucket public repo
      * @return BitBucketConfigureRepositoriesPage
      */
+    @SuppressWarnings("unchecked")
+    @Override
     public GithubConfigureRepositoriesPage addPrivateRepoToProjectSuccessfully(String projectKey, String url)
     {
         projectSelect.select(Options.value(projectKey));
         urlTextbox.clear().type(url);
         addRepositoryButton.click();
-        Poller.waitUntil(addedRepositoryH2.timed().getText(), AnyOf.anyOf(new IsEqual<String>("New Bitbucket repository"), new IsEqual<String>("New Github repository")));
-        if(messageBarDiv.isPresent()){
+        Poller.waitUntil(addedRepositoryH2.timed().getText(), AnyOf.anyOf(new IsEqual<String>("New Bitbucket repository"),
+            new IsEqual<String>("New Github repository")));
+        if (messageBarDiv.isPresent())
+        {
             PageElement messageBarErrorDiv = messageBarDiv.find(By.className("error"));
-            if(messageBarErrorDiv.isPresent() && messageBarErrorDiv.timed().getText().now().contains("OAuth needs to be")){
+            if (messageBarErrorDiv.isPresent() && messageBarErrorDiv.timed().getText().now().contains("OAuth needs to be"))
+            {
                 PageElement oauthLink = messageBarErrorDiv.find(By.linkText("configured before adding private github repository."));
-                if(oauthLink.isPresent()){
+                if (oauthLink.isPresent())
+                {
                     String currentUrl = jiraTestedProduct.getTester().getDriver().getCurrentUrl();
                     jiraTestedProduct.getTester().getDriver().navigate().to(oauthLink.getAttribute("href"));
                     Poller.waitUntilTrue("Expected OAuth form to appear.", ghClientID.timed().isVisible());
                     ghClientID.type("281f79035707e14feebe");
                     ghClientSecret.type("2dd08e37c903782cc6e38a6a25b572887b6d5583");
                     addRepositoryButton.click();
-                    Poller.waitUntilTrue("Expected sync status message to be 'GitHub Client Identifiers Set Correctly'", ghMessagesDiv.find(By.tagName("h2")).timed().hasText("GitHub Client Identifiers Set Correctly"));
+                    Poller.waitUntilTrue("Expected sync status message to be 'GitHub Client Identifiers Set Correctly'", ghMessagesDiv
+                        .find(By.tagName("h2")).timed().hasText("GitHub Client Identifiers Set Correctly"));
                     jiraTestedProduct.getTester().getDriver().navigate().to(currentUrl);
-                    Poller.waitUntil(addedRepositoryH2.timed().getText(), AnyOf.anyOf(new IsEqual<String>("New Bitbucket repository"), new IsEqual<String>("New Github repository")));
+                    Poller.waitUntil(addedRepositoryH2.timed().getText(), AnyOf.anyOf(new IsEqual<String>("New Bitbucket repository"),
+                        new IsEqual<String>("New Github repository")));
 
                 }
             }
