5d6e334d7a4b2bdc0b17ed7c4504caf605c27283
dusanhornik <devnull@localhost>
2011-12-08 15:46:54 +0100

    BBC-36 IssueLinker  - component for generating html links from issueKeys

---
+++ src/test/java/com/atlassian/jira/plugins/bitbucket/TestIssueLinker.java
@@ -0,0 +1,37 @@
+package com.atlassian.jira.plugins.bitbucket;
+
+import static junit.framework.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.atlassian.sal.api.ApplicationProperties;
+
+public class TestIssueLinker
+{
+    @Mock
+    ApplicationProperties applicationProperty;
+    
+    @Before
+    public void setup() throws Exception
+    {
+        MockitoAnnotations.initMocks(this);
+        when(applicationProperty.getBaseUrl()).thenReturn("http://jirabaseurl/jira");
+    }
+    
+    @Test
+    public void anonymous()
+    {
+        IssueLinker issueLinker = new IssueLinkerImpl(applicationProperty);
+        
+        assertEquals("<a href=\"http://jirabaseurl/jira/browse/XXX-1\">XXX-1</a>",issueLinker.createLinks("XXX-1"));
+        assertEquals("<a href=\"http://jirabaseurl/jira/browse/XXX-1\">XXX-1</a> <a href=\"http://jirabaseurl/jira/browse/XYZ-1\">XYZ-1</a>",issueLinker.createLinks("XXX-1 XYZ-1"));
+        assertEquals(
+            "Fixing issues <a href=\"http://jirabaseurl/jira/browse/XXX-1\">XXX-1</a>,<a href=\"http://jirabaseurl/jira/browse/XXX-2\">XXX-2</a> and <a href=\"http://jirabaseurl/jira/browse/XX-3\">XX-3</a>, done.",
+            issueLinker.createLinks("Fixing issues XXX-1,XXX-2 and XX-3, done."));
+        
+    }
+}
