221ad158f2bdf9d546594d2fb22bfba2ec2184e2
komo <komo@komo-eea>
2011-12-16 15:43:23 +0100

    BBC-58 fix to show commits on 'All' issue tab

--- src/main/java/com/atlassian/jira/plugins/bitbucket/webwork/BitbucketTabPanel.java
+++ src/main/java/com/atlassian/jira/plugins/bitbucket/webwork/BitbucketTabPanel.java
@@ -1,12 +1,6 @@
 package com.atlassian.jira.plugins.bitbucket.webwork;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
-
+import com.opensymphony.user.User;
 import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.issue.tabpanels.GenericMessageAction;
 import com.atlassian.jira.plugin.issuetabpanel.AbstractIssueTabPanel;
@@ -16,15 +10,19 @@ import com.atlassian.jira.plugins.bitbucket.api.SourceControlRepository;
 import com.atlassian.jira.plugins.bitbucket.spi.RepositoryManager;
 import com.atlassian.jira.security.PermissionManager;
 import com.atlassian.jira.security.Permissions;
-import com.opensymphony.user.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 
-@SuppressWarnings("deprecation")
+import java.util.ArrayList;
+import java.util.List;
+
 public class BitbucketTabPanel extends AbstractIssueTabPanel
 {
     private static final GenericMessageAction DEFAULT_MESSAGE = new GenericMessageAction("");
     private final PermissionManager permissionManager;
     private final Logger logger = LoggerFactory.getLogger(BitbucketTabPanel.class);
-	private final RepositoryManager globalRepositoryManager;
+    private final RepositoryManager globalRepositoryManager;
 
     public BitbucketTabPanel(PermissionManager permissionManager, @Qualifier("globalRepositoryManager") RepositoryManager globalRepositoryManager)
     {
@@ -42,12 +40,11 @@ public class BitbucketTabPanel extends AbstractIssueTabPanel
             for (Changeset changeset : globalRepositoryManager.getChangesets(issueId))
             {
                 logger.debug("found changeset [ {} ] on issue [ {} ]", changeset.getNode(), issueId);
-				SourceControlRepository repository = globalRepositoryManager.getRepository(changeset.getRepositoryId());
-				String changesetAsHtml = globalRepositoryManager.getHtmlForChangeset(repository, changeset);
-                bitbucketActions.add(new GenericMessageAction(changesetAsHtml));
+                SourceControlRepository repository = globalRepositoryManager.getRepository(changeset.getRepositoryId());
+                String changesetAsHtml = globalRepositoryManager.getHtmlForChangeset(repository, changeset);
+                bitbucketActions.add(new CommitsIssueAction(changesetAsHtml, changeset.getTimestamp()));
             }
-        }
-        catch (com.atlassian.jira.plugins.bitbucket.api.SourceControlException e)
+        } catch (com.atlassian.jira.plugins.bitbucket.api.SourceControlException e)
         {
             logger.debug("Could not retrieve changeset for [ " + issueId + " ]: " + e, e);
         }
@@ -58,7 +55,7 @@ public class BitbucketTabPanel extends AbstractIssueTabPanel
         return bitbucketActions;
     }
 
-	@Override
+    @Override
     public boolean showPanel(Issue issue, User user)
     {
         return permissionManager.hasPermission(Permissions.VIEW_VERSION_CONTROL, issue, user) &&
