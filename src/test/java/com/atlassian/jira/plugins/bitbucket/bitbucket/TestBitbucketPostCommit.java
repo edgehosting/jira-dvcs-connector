0371186fe4b5907cdcb7560d99f3208eec7784c3
komo <komo@komo-eea>
2012-01-04 10:58:21 +0100

    fixed todo: fixed commented unit test for paylod parsing

--- src/test/java/com/atlassian/jira/plugins/bitbucket/bitbucket/TestBitbucketPostCommit.java
+++ src/test/java/com/atlassian/jira/plugins/bitbucket/bitbucket/TestBitbucketPostCommit.java
@@ -1,22 +1,33 @@
 package com.atlassian.jira.plugins.bitbucket.bitbucket;
 
-import static org.mockito.Mockito.*;
+import com.atlassian.jira.plugins.bitbucket.Synchronizer;
+import com.atlassian.jira.plugins.bitbucket.api.Changeset;
+import com.atlassian.jira.plugins.bitbucket.api.SourceControlRepository;
+import com.atlassian.jira.plugins.bitbucket.api.impl.DefaultSourceControlRepository;
+import com.atlassian.jira.plugins.bitbucket.spi.Communicator;
+import com.atlassian.jira.plugins.bitbucket.spi.RepositoryManager;
+import com.atlassian.jira.plugins.bitbucket.spi.RepositoryUri;
+import com.atlassian.jira.plugins.bitbucket.spi.bitbucket.impl.BitbucketRepositoryManager;
+import com.atlassian.jira.plugins.bitbucket.spi.github.MinimalInfoChangeset;
+import com.atlassian.jira.plugins.bitbucket.webwork.BitbucketPostCommit;
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentMatcher;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 
-import org.apache.commons.io.IOUtils;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
-import com.atlassian.jira.plugins.bitbucket.Synchronizer;
-import com.atlassian.jira.plugins.bitbucket.api.SourceControlRepository;
-import com.atlassian.jira.plugins.bitbucket.api.impl.DefaultSourceControlRepository;
-import com.atlassian.jira.plugins.bitbucket.spi.RepositoryManager;
-import com.atlassian.jira.plugins.bitbucket.spi.RepositoryUri;
-import com.atlassian.jira.plugins.bitbucket.webwork.BitbucketPostCommit;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Unit test for {@link BitbucketPostCommit}
@@ -27,9 +38,11 @@ public class TestBitbucketPostCommit
     @Mock
     Synchronizer synchronizer;
     @Mock
-	private RepositoryManager repositoryManager;
+    RepositoryManager repositoryManager;
     @Mock
     RepositoryUri repositoryUri;
+    @Mock
+    Communicator communicator;
 
 
     @Before
@@ -46,44 +59,55 @@ public class TestBitbucketPostCommit
     @Test
     public void testPostCommit() throws Exception
     {
-    	String projectKey = "PRJ";
-    	String repositoryUrl = "https://bitbucket.org/mjensen/test";
-    	String payload = resource("TestBitbucketPostCommit-payload.json");
+        String projectKey = "PRJ";
+        String repositoryUrl = "https://bitbucket.org/mjensen/test";
+        String payload = resource("TestBitbucketPostCommit-payload.json");
         SourceControlRepository repo = new DefaultSourceControlRepository(0, "bitbucket", repositoryUri, projectKey, null, null, null, null, null);
-    	when(repositoryManager.getRepositories(projectKey)).thenReturn(Arrays.asList(repo));
+        when(repositoryManager.getRepositories(projectKey)).thenReturn(Arrays.asList(repo));
         when(repositoryUri.getRepositoryUrl()).thenReturn(repositoryUrl);
 
-		BitbucketPostCommit bitbucketPostCommit = new BitbucketPostCommit(repositoryManager, synchronizer);
-		bitbucketPostCommit.setProjectKey(projectKey);
-		bitbucketPostCommit.setPayload(payload);
+        BitbucketPostCommit bitbucketPostCommit = new BitbucketPostCommit(repositoryManager, synchronizer);
+        bitbucketPostCommit.setProjectKey(projectKey);
+        bitbucketPostCommit.setPayload(payload);
         bitbucketPostCommit.execute();
-		verify(repositoryManager).parsePayload(repo, payload);
+        verify(repositoryManager).parsePayload(repo, payload);
     }
 
     @Test
     public void testParsePayload() throws Exception
-	{
-        // TODO
-//    	String projectKey = "PRJ";
-//    	String payload = resource("TestBitbucketPostCommit-payload.json");
-//    	DefaultSourceControlRepository repo = new DefaultSourceControlRepository(0, "bitbucket", repositoryUri, projectKey, null, null, null, null, null);
-//
-//    	BitbucketRepositoryManager brm = new BitbucketRepositoryManager(null, null, null, null, null);
-//		List<Changeset> changesets = brm.parsePayload(repo, payload);
-//    	
-//        ArgumentMatcher<List<Changeset>> matcher = new ArgumentMatcher<List<Changeset>>()
-//		{
-//		    @Override
-//            public boolean matches(Object o)
-//		    {
-//		        //noinspection unchecked
-//		        @SuppressWarnings("unchecked")
-//                List<Changeset> list = (List<Changeset>) o;
-//		        Changeset changeset = list.get(0);
-//		        return list.size()==1 && changeset.getNode().equals("f2851c9f1db8");
-//		    }
-//		};
-//		assertTrue(matcher.matches(changesets));
+    {
+        final String projectKey = "PRJ";
+        final String payload = resource("TestBitbucketPostCommit-payload.json");
+        final String node = "f2851c9f1db8";
+        final DefaultSourceControlRepository repo = new DefaultSourceControlRepository(0, "bitbucket", repositoryUri, projectKey, null, null, null, null, null);
+
+        BitbucketRepositoryManager brm = new BitbucketRepositoryManager(null, communicator, null, null, null, null);
+
+        when(communicator.getChangeset(eq(repo), anyString())).thenAnswer(new Answer<Object>()
+        {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable
+            {
+                return new MinimalInfoChangeset(0, (String) invocation.getArguments()[1], null);
+            }
+        });
+
+        List<Changeset> changesets = brm.parsePayload(repo, payload);
+
+        ArgumentMatcher<List<Changeset>> matcher = new ArgumentMatcher<List<Changeset>>()
+        {
+            @Override
+            public boolean matches(Object o)
+            {
+                //noinspection unchecked
+                @SuppressWarnings("unchecked")
+                List<Changeset> list = (List<Changeset>) o;
+                Changeset changeset = list.get(0);
+                return list.size() == 1 && changeset.getNode().equals(node);
+            }
+        };
+        Assert.assertTrue(matcher.matches(changesets));
+        verify(communicator);
     }
 
 }
