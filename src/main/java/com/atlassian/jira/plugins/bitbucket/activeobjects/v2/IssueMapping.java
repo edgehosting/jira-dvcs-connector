f906cf151904676fc0eb99b6c5cb4ecd4843d0a0
dusanhornik <devnull@localhost>
2012-01-26 21:42:37 +0100

    Introducing constants for ProjectMapping column names

--- src/main/java/com/atlassian/jira/plugins/bitbucket/activeobjects/v2/IssueMapping.java
+++ src/main/java/com/atlassian/jira/plugins/bitbucket/activeobjects/v2/IssueMapping.java
@@ -7,20 +7,20 @@ import net.java.ao.schema.Table;
 import java.util.Date;
 
 @Table("IssueMappingV2")
-public interface IssueMapping extends Entity {
-
-    public static final String COLUMN_REPOSITORY_ID = "REPOSITORY_ID";
-    public static final String COLUMN_ISSUE_ID = "ISSUE_ID";
-    public static final String COLUMN_NODE = "NODE";
-    public static final String COLUMN_RAW_AUTHOR = "RAW_AUTHOR";
-    public static final String COLUMN_AUTHOR = "AUTHOR";
-        public static final String COLUMN_DATE = "DATE";
-    public static final String COLUMN_RAW_NODE = "RAW_NODE";
-    public static final String COLUMN_BRANCH = "BRANCH";
-    public static final String COLUMN_MESSAGE = "MESSAGE";
-    public static final String COLUMN_PARENTS_DATA = "PARENTS_DATA";
-    public static final String COLUMN_FILES_DATA = "FILES_DATA";
-    public static final String COLUMN_VERSION = "VERSION";
+public interface IssueMapping extends Entity
+{
+    public static final String REPOSITORY_ID = "REPOSITORY_ID";
+    public static final String ISSUE_ID = "ISSUE_ID";
+    public static final String NODE = "NODE";
+    public static final String RAW_AUTHOR = "RAW_AUTHOR";
+    public static final String AUTHOR = "AUTHOR";
+    public static final String DATE = "DATE";
+    public static final String RAW_NODE = "RAW_NODE";
+    public static final String BRANCH = "BRANCH";
+    public static final String MESSAGE = "MESSAGE";
+    public static final String PARENTS_DATA = "PARENTS_DATA";
+    public static final String FILES_DATA = "FILES_DATA";
+    public static final String VERSION = "VERSION";
     /**
      * Rows at the table can contain data loaded by previous versions of this plugin. Some column data maybe missing 
      * because previous versions of plugin was not loading them. To get the updated version of changeset we need 
@@ -29,54 +29,33 @@ public interface IssueMapping extends Entity {
     public static final int LATEST_VERSION = 2;
 
     int getRepositoryId();
-
     String getNode();
-
     String getIssueId();
-
     String getRawAuthor();
-
     String getAuthor();
-
     Date getDate();
-
     String getRawNode();
-
     String getBranch();
-
     @StringLength(StringLength.UNLIMITED)
     String getMessage();
-
     @StringLength(StringLength.UNLIMITED)
     String getFilesData();
-
     String getParentsData();
-
     Integer getVersion();
 
 
     void setRepositoryId(int repositoryId);
-
     void setNode(String node);
-
     void setIssueId(String issueId);
-
     void setRawAuthor(String rawAuthor);
-
     void setAuthor(String author);
-
     void setDate(Date date);
-
     void setRawNode(String rawNode);
-
     void setBranch(String branch);
-
+    @StringLength(StringLength.UNLIMITED)
     void setMessage(String message);
-
     @StringLength(StringLength.UNLIMITED)
     void setFilesData(String files);
-
     void setParentsData(String parents);
-
     void setVersion(Integer version);
 }
--- src/main/java/com/atlassian/jira/plugins/bitbucket/activeobjects/v2/ProjectMapping.java
+++ src/main/java/com/atlassian/jira/plugins/bitbucket/activeobjects/v2/ProjectMapping.java
@@ -8,6 +8,16 @@ import net.java.ao.schema.Table;
 @Table("ProjectMappingV2")
 public interface ProjectMapping extends Entity
 {
+    public static final String ACCESS_TOKEN = "ACCESS_TOKEN";
+    public static final String PASSWORD = "PASSWORD";
+    public static final String USERNAME = "USERNAME";
+    public static final String ADMIN_PASSWORD = "ADMIN_PASSWORD";
+    public static final String ADMIN_USERNAME = "ADMIN_USERNAME";
+    public static final String REPOSITORY_TYPE = "REPOSITORY_TYPE";
+    public static final String PROJECT_KEY = "PROJECT_KEY";
+    public static final String REPOSITORY_URL = "REPOSITORY_URL";
+    public static final String REPOSITORY_NAME = "REPOSITORY_NAME";
+    
     String getRepositoryName();
     String getRepositoryType();
     String getRepositoryUrl();
