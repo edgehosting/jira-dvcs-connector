e5393adfd7523cafe572e7d0f1ce9bd3138d3caa
miroslavfarkas <devnull@localhost>
2012-04-27 11:02:00 +0200

    BBC-152: create V3 model for supporting concept of Organizations in DVCS

---
+++ jira-dvcs-connector-api/src/main/java/com/atlassian/jira/plugins/bitbucket/activeobjects/v3/OrganizationMapping.java
@@ -0,0 +1,30 @@
+package com.atlassian.jira.plugins.bitbucket.activeobjects.v3;
+
+import net.java.ao.Entity;
+import net.java.ao.schema.Table;
+
+@Table("OrganizationMapping")
+public interface OrganizationMapping extends Entity
+{
+
+    public static final String HOST_URL = "HOST_URL";
+    public static final String NAME = "NAME";
+    public static final String DVCS_TYPE = "DVCS_TYPE";
+    public static final String ADMIN_USERNAME = "ADMIN_USERNAME";
+    public static final String ADMIN_PASSWORD = "ADMIN_PASSWORD";
+    public static final String ACCESS_TOKEN = "ACCESS_TOKEN";
+
+    String getHostUrl();
+    String getName();
+    String getDvcsType();
+    String getAdminUsername();
+    String getAdminPassword();
+    String getAccessToken();
+
+    void setHostUrl(String hostUrl);
+    void setName(String name);
+    void setDvcsType(String dvcsType);
+    void setAdminUsername(String adminUsername);
+    void setAdminPassword(String adminPassword);
+    void setAccessToken(String accessToken);
+}
---
+++ jira-dvcs-connector-api/src/main/java/com/atlassian/jira/plugins/bitbucket/activeobjects/v3/RepositoryMapping.java
@@ -0,0 +1,24 @@
+package com.atlassian.jira.plugins.bitbucket.activeobjects.v3;
+
+import net.java.ao.schema.Table;
+
+import java.util.Date;
+
+@Table("RepositoryMapping")
+public interface RepositoryMapping
+{
+    public static final String ORGANIZATION_ID = "ORGANIZATION_ID";
+    public static final String NAME = "NAME";
+    public static final String LAST_COMMIT_DATE = "LAST_COMMIT_DATE";
+    public static final String LINKED = "LINKED";
+
+    int getOrganizationId();
+    String getName();
+    Date getLastCommitDate();
+    boolean isLinked();
+
+    void setOrganizationId(int organizationId);
+    void setName(String name);
+    void setLastCommitDate(Date lastCommitDate);
+    void setLinked(boolean linked);
+}
