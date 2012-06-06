609b6890ef71c3a4ad32c4ccbeee85fe9c119699
hg <devnull@localhost>
2012-06-06 22:49:39 +0200

    JRADEV-12475 BBC-178 Transplanting layout fix from default branch: https://bitbucket.org/atlassian/jira-bitbucket-connector/changeset/bac29dc22403

--- jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/bitbucket/webwork/CommitsIssueAction.java
+++ jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/bitbucket/webwork/CommitsIssueAction.java
@@ -1,17 +1,25 @@
 package com.atlassian.jira.plugins.bitbucket.webwork;
 
 import com.atlassian.jira.issue.tabpanels.GenericMessageAction;
+import com.atlassian.jira.plugin.issuetabpanel.IssueAction;
 
 import java.util.Date;
 
-public class CommitsIssueAction extends GenericMessageAction
+public class CommitsIssueAction implements IssueAction
 {
-    private Date timestamp;
+    private final String htmlContent;
+    private final Date timestamp;
 
     public CommitsIssueAction(String changesetAsHtml, Date timestamp)
     {
-        super(changesetAsHtml);
+        htmlContent = changesetAsHtml;
         this.timestamp = timestamp;
+    }
+
+    @Override
+    public String getHtml()
+    {
+        return htmlContent;
     }
 
     @Override
