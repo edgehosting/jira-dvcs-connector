e7d1b5afe9acbd868df4b56d5d4374da1734ae70
Martin Skurla <devnull@localhost>
2012-06-11 12:24:31 +0200

    BBC-175 - added test for setting POST commit hook on Github

--- jira-dvcs-connector-github/src/test/java/com/atlassian/jira/plugins/dvcs/github/GithubCommunicatorTest.java
+++ jira-dvcs-connector-github/src/test/java/com/atlassian/jira/plugins/dvcs/github/GithubCommunicatorTest.java
@@ -1,21 +1,15 @@
 package com.atlassian.jira.plugins.dvcs.github;
 
-import static org.mockito.Mockito.*;
 
-import java.io.IOException;
-
-import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import com.atlassian.jira.plugins.dvcs.auth.AuthenticationFactory;
-import com.atlassian.jira.plugins.dvcs.model.Credential;
-import com.atlassian.jira.plugins.dvcs.model.Organization;
 import com.atlassian.jira.plugins.dvcs.model.Repository;
 import com.atlassian.jira.plugins.dvcs.net.DefaultRequestHelper;
-import com.atlassian.jira.plugins.dvcs.net.ExtendedResponseHandler;
 import com.atlassian.jira.plugins.dvcs.net.ExtendedResponseHandlerFactory;
 import com.atlassian.jira.plugins.dvcs.service.ChangesetCache;
 import com.atlassian.jira.plugins.dvcs.service.remote.DvcsCommunicator;
@@ -24,86 +18,88 @@ import com.atlassian.jira.plugins.dvcs.spi.github.GithubOAuth;
 import com.atlassian.sal.api.net.Request;
 import com.atlassian.sal.api.net.RequestFactory;
 
+import static org.mockito.Mockito.*;
+
+
+/**
+ * @author Martin Skurla
+ */
+@RunWith(MockitoJUnitRunner.class)
 public class GithubCommunicatorTest
 {
 
 	@Mock
 	private AuthenticationFactory authenticationFactory;
-	
+
 	@Mock
 	private ExtendedResponseHandlerFactory extendedResponseHandlerFactory;
 
 	@Mock
-	private ExtendedResponseHandler responseHandler;
-	
-	@Mock
 	private Request<?, ?> request;
-	
+
 	@Mock
 	private Repository repository;
-	
+
 	@SuppressWarnings("rawtypes")
 	@Mock
 	private RequestFactory requestFactory;
-	
+
 	@Mock
 	private ChangesetCache changesetCache;
 
 	@Mock
-	private GithubOAuth githubOAuth;
-	
+	private GithubOAuth githubOAuth;//TODO pomazat mocky ktore tam nemaju byt, ...
+
 	// tested object
 	private DvcsCommunicator communicator;
 
-	
-	
-	public GithubCommunicatorTest()
-	{
-		super();
-	}
 
 	@Before
-	public void setup() {
-		
-		MockitoAnnotations.initMocks(this);
-		
-		communicator = new GithubCommunicator(changesetCache, new DefaultRequestHelper(requestFactory, extendedResponseHandlerFactory), authenticationFactory, githubOAuth);
-		
-		when(extendedResponseHandlerFactory.create()).thenReturn(responseHandler);
+	public void initializeGithubCommunicator() {
+		communicator = new GithubCommunicator(changesetCache,
+                                              new DefaultRequestHelper(requestFactory, extendedResponseHandlerFactory),
+                                              authenticationFactory,
+                                              githubOAuth);
 	}
-	
+
 	@Test
-	public void testGetRepositories() {
-		
-	
+	public void testSetupPostcommitHook() {
+
+		when(requestFactory.createRequest(any(Request.MethodType.class), anyString())).thenReturn(request);
+		when(repository.getOrgName()).thenReturn("org");
+		when(repository.getSlug()).thenReturn("slug");
+		when(repository.getOrgHostUrl()).thenReturn(""); // is not important
+
+		communicator.setupPostcommitHook(repository, "post-commit-url");
+
+		verify(requestFactory).createRequest(eq(Request.MethodType.POST),
+                                             eq("https://api.github.com/repos/org/slug/hooks"));
+        verify(request).setRequestBody(contains("post-commit-url"));
 	}
-	
-	@Test
-	public void testSetupPostcommitHook () {
-		
-	}
-	
-	private Organization createSampleOrganization()
-	{
-		Organization organization = new Organization();
-		organization.setDvcsType("bitbucket");
-		organization.setHostUrl("https://bitbucket.org");
-		organization.setName("doesnotmatter");
-		organization.setCredential(new Credential("doesnotmatter_u", "doesnotmatter_p", null));
-		return organization;
-	}
-	
-	private String resource(String name)
-	{
-		try
-		{
-			
-			return IOUtils.toString(getClass().getClassLoader().getResourceAsStream(name));
-			
-		} catch (IOException e)
-		{
-			throw new RuntimeException("Can not load resource " + name, e);
-		}
-	}
+
+//	private Organization createSampleOrganization()
+//	{
+//		Organization organization = new Organization();
+//
+//        organization.setDvcsType("github");
+//		organization.setHostUrl ("https://github.com");
+//		organization.setName    ("doesnotmatter");
+//
+//        organization.setCredential(new Credential("doesnotmatter_u", "doesnotmatter_p", null));
+//
+//        return organization;
+//	}
+//
+//	private static String resourceAsString(String name)
+//	{
+//		try
+//		{
+//			return IOUtils.toString(GithubCommunicatorTest.class.getClassLoader().getResourceAsStream(name));
+//
+//		} catch (IOException e)
+//		{
+//			throw new RuntimeException("Can not load resource " + name, e);
+//		}
+//	}
 }
 
