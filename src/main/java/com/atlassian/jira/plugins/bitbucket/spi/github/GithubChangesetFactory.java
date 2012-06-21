aff43fcd93630b55c61d70deca45d6fecae289aa
Martin Skurla <devnull@localhost>
2012-06-21 09:30:35 +0200

    BBC-199 - GithubChangesetFactory can now transform Github RepositoryCommit into DVCS Changeset

--- src/main/java/com/atlassian/jira/plugins/bitbucket/spi/github/GithubChangesetFactory.java
+++ src/main/java/com/atlassian/jira/plugins/bitbucket/spi/github/GithubChangesetFactory.java
@@ -1,20 +1,29 @@
 package com.atlassian.jira.plugins.bitbucket.spi.github;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import org.eclipse.egit.github.core.Commit;
+import org.eclipse.egit.github.core.CommitFile;
+import org.eclipse.egit.github.core.RepositoryCommit;
+
 import com.atlassian.jira.plugins.bitbucket.api.Changeset;
 import com.atlassian.jira.plugins.bitbucket.api.ChangesetFile;
 import com.atlassian.jira.plugins.bitbucket.api.SourceControlException;
 import com.atlassian.jira.plugins.bitbucket.spi.CustomStringUtils;
-import com.atlassian.jira.plugins.bitbucket.spi.DefaultChangeset;
 import com.atlassian.jira.plugins.bitbucket.spi.DefaultBitbucketChangesetFile;
+import com.atlassian.jira.plugins.bitbucket.spi.DefaultChangeset;
 import com.atlassian.jira.util.json.JSONArray;
 import com.atlassian.jira.util.json.JSONException;
 import com.atlassian.jira.util.json.JSONObject;
+import com.atlassian.util.concurrent.Nullable;
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
 
 import javax.xml.bind.DatatypeConverter;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
 
 /**
  * Factory for {@link com.atlassian.jira.plugins.bitbucket.api.Changeset} implementations
@@ -25,7 +34,7 @@ public class GithubChangesetFactory
     /**
      * Parse the json object from GitHub v2. API as a changeset. We need only minimal information from that.
      *
-     * @param json  the json object describing the change
+     * @param commitJson  the json object describing the change
      * @return the parsed {@link com.atlassian.jira.plugins.bitbucket.api.Changeset} with minimal fields
      */
     public static Changeset parseV2(int repositoryId, JSONObject commitJson) throws JSONException
@@ -136,4 +145,67 @@ public class GithubChangesetFactory
     private GithubChangesetFactory()
     {
     }
+
+    public static Changeset transform(RepositoryCommit repositoryCommit, int repositoryId, String branch)
+    {
+        final List<ChangesetFile> changesetFiles = transformFiles(repositoryCommit.getFiles());
+
+        return new DefaultChangeset(
+                repositoryId,
+                repositoryCommit.getSha(),
+                repositoryCommit.getCommit().getAuthor().getName(),
+                repositoryCommit.getAuthor().getLogin(),
+                repositoryCommit.getCommit().getAuthor().getDate(),
+                "", // todo: raw-node. what is it in github?
+                branch,
+                repositoryCommit.getCommit().getMessage(),
+                transformParents(repositoryCommit.getParents()),
+                changesetFiles,
+                changesetFiles.size()
+        );
+    }
+
+
+    private static List<ChangesetFile> transformFiles(List<CommitFile> files)
+    {
+        if (files == null)
+        {
+            return Collections.<ChangesetFile>emptyList();
+        }
+
+        return Lists.transform(files, new Function<CommitFile, ChangesetFile>()
+        {
+            @Override
+            public ChangesetFile apply(@Nullable CommitFile commitFile)
+            {
+                String filename = commitFile.getFilename();
+                String status = commitFile.getStatus();
+                int additions = commitFile.getAdditions();
+                int deletions = commitFile.getDeletions();
+
+                return new DefaultBitbucketChangesetFile(CustomStringUtils.getChangesetFileAction(status),
+                        filename, additions, deletions);
+            }
+        });
+    }
+
+
+    private static List<String> transformParents(List<Commit> parents)
+    {
+        if (parents == null)
+        {
+            return Collections.<String>emptyList();
+        }
+
+        return Lists.transform(parents, new Function<Commit, String>()
+        {
+            @Override
+            public String apply(@Nullable Commit commit)//TODO ???
+            {
+                return commit.getSha();
+            }
+        });
+
+    }
+
 }
