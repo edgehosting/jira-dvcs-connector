31b25f4ee505fca2185b27f938bdcdce78eabb98
Martin Skurla <mskurla@atlassian.com>
2012-07-17 17:36:39 +0200

    BBC-217 - getting changesets by given start node and limit as well as getting all changesets lazily added

--- bitbucket-client/src/main/java/com/atlassian/jira/plugins/dvcs/spi/bitbucket/clientlibrary/restpoints/ChangesetRemoteRestpoint.java
+++ bitbucket-client/src/main/java/com/atlassian/jira/plugins/dvcs/spi/bitbucket/clientlibrary/restpoints/ChangesetRemoteRestpoint.java
@@ -1,11 +1,14 @@
 package com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.restpoints;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.client.ClientUtils;
 import com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.model.BitbucketChangeset;
+import com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.model.BitbucketChangesetEnvelope;
 import com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.model.BitbucketChangesetWithDiffstat;
 import com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.request.RemoteRequestor;
 import com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.request.RemoteResponse;
@@ -18,6 +21,7 @@ import com.google.gson.reflect.TypeToken;
  */
 public class ChangesetRemoteRestpoint
 {
+    private static final int DEFAULT_CHANGESETS_LIMIT = 5;
     private static final int DIFFSTAT_NO_LIMIT = -1;
     
     private final RemoteRequestor requestor;
@@ -57,6 +61,83 @@ public class ChangesetRemoteRestpoint
         
         return ClientUtils.fromJson(response.getResponse(),
                                     new TypeToken<List<BitbucketChangesetWithDiffstat>>(){}.getType());
+    }
+    
+    public Iterable<BitbucketChangeset> getChangesets(final String owner, final String slug)
+    {
+        return new Iterable<BitbucketChangeset>() {
+
+            @Override
+            public Iterator<BitbucketChangeset> iterator()
+            {
+                return new BitbucketChangesetIterator(owner, slug);
+            }
+        };
+    }
+    
+    public List<BitbucketChangeset> getChangesets(String owner, String slug, String startNode, int limit)
+    {
+        String getChangesetsWithPageAndLimitUrl = String.format("/repositories/%s/%s/changesets", owner, slug);
+
+        Map<String, String> parameters = new HashMap<String, String>();
+        parameters.put("start", startNode);
+        parameters.put("limit", "" + limit);
+        
+        RemoteResponse response = requestor.get(getChangesetsWithPageAndLimitUrl, parameters);
+        
+        BitbucketChangesetEnvelope bitbucketChangesetEnvelope = ClientUtils.fromJson(response.getResponse(),
+                                                                                     BitbucketChangesetEnvelope.class);
+        
+        return bitbucketChangesetEnvelope.getChangesets();
+    }
+    
+    private final class BitbucketChangesetIterator implements Iterator<BitbucketChangeset> {
+        private final String owner;
+        private final String slug;
+        
+        private Iterator<BitbucketChangeset> changesetsCurrentPage;
+        private BitbucketChangeset firstChangesetInPage;
+
+        
+        private BitbucketChangesetIterator(String owner, String slug)
+        {
+            this.owner = owner;
+            this.slug = slug;
+            
+            List<BitbucketChangeset> changesets = getChangesets(owner, slug, "tip", DEFAULT_CHANGESETS_LIMIT);
+            firstChangesetInPage = changesets.get(0);
+            changesetsCurrentPage = changesets.iterator();
+        }
+
+        
+        @Override
+        public boolean hasNext()
+        {
+            return changesetsCurrentPage.hasNext() || hasMorePages();
+        }
+
+        @Override
+        public BitbucketChangeset next()
+        {
+            return changesetsCurrentPage.next();
+        }
+
+        @Override
+        public void remove()
+        {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+        
+        private boolean hasMorePages()
+        {
+            List<BitbucketChangeset> changesets = getChangesets(owner, slug, firstChangesetInPage.getNode(), DEFAULT_CHANGESETS_LIMIT);
+            firstChangesetInPage = changesets.get(0);
+            
+            changesets.remove(changesets.size() - 1);
+            changesetsCurrentPage = changesets.iterator();
+            
+            return !changesets.isEmpty();
+        }
     }
 }
 
