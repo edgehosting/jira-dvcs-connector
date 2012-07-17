16677efb8455cdfc18096dff84f0afd393c7fcde
Martin Skurla <mskurla@atlassian.com>
2012-07-17 10:44:29 +0200

    BBC-217 - getting changeset support

--- bitbucket-client/src/main/java/com/atlassian/jira/plugins/dvcs/spi/bitbucket/clientlibrary/restpoints/ChangesetRemoteRestpoint.java
+++ bitbucket-client/src/main/java/com/atlassian/jira/plugins/dvcs/spi/bitbucket/clientlibrary/restpoints/ChangesetRemoteRestpoint.java
@@ -1,11 +1,33 @@
 package com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.restpoints;
 
+import com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.client.ClientUtils;
+import com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.model.BitbucketChangeset;
+import com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.request.RemoteRequestor;
+import com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.request.RemoteResponse;
+
+/**
+ * ChangesetRemoteRestpoint
+ * 
+ * @author Martin Skurla mskurla@atlassian.com
+ */
 public class ChangesetRemoteRestpoint
 {
+    private final RemoteRequestor requestor;
+    
 
-	public ChangesetRemoteRestpoint()
+	public ChangesetRemoteRestpoint(RemoteRequestor remoteRequestor)
 	{
-		super();
+		this.requestor = remoteRequestor;
 	}
+    
+    
+    public BitbucketChangeset getChangeset(String owner, String slug, String node)
+    {
+        String getChangesetUrl = String.format("/repositories/%s/%s/changesets/%s", owner, slug, node);
+        
+        RemoteResponse response = requestor.get(getChangesetUrl, null);
+        
+        return ClientUtils.fromJson(response.getResponse(), BitbucketChangeset.class);
+    }
 }
 
