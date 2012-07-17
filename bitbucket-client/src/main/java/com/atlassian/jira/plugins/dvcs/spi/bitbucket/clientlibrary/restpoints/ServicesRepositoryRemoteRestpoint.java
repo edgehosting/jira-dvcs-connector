d5a82cae5139bff9e672ca5699352ee10fd4782b
Martin Skurla <mskurla@atlassian.com>
2012-07-17 11:09:23 +0200

    BBC-217 - adding POST service support

--- bitbucket-client/src/main/java/com/atlassian/jira/plugins/dvcs/spi/bitbucket/clientlibrary/restpoints/ServicesRepositoryRemoteRestpoint.java
+++ bitbucket-client/src/main/java/com/atlassian/jira/plugins/dvcs/spi/bitbucket/clientlibrary/restpoints/ServicesRepositoryRemoteRestpoint.java
@@ -1,11 +1,35 @@
 package com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.restpoints;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.client.ClientUtils;
+import com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.model.BitbucketService;
+import com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.request.RemoteRequestor;
+import com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.request.RemoteResponse;
+
 public class ServicesRepositoryRemoteRestpoint
 {
+    private RemoteRequestor requestor;
+    
 
-	public ServicesRepositoryRemoteRestpoint()
+	public ServicesRepositoryRemoteRestpoint(RemoteRequestor remoteRequestor)
 	{
-		super();
+		this.requestor = remoteRequestor;
 	}
+    
+    
+    public BitbucketService addPOSTService(String owner, String slug, String serviceUrl)
+    {
+        String addServiceUrl = String.format("/repositories/%s/%s/services", owner, slug);
+        
+        Map<String, String> addServiceParameters = new HashMap<String, String>();
+        addServiceParameters.put("type", "POST");
+        addServiceParameters.put("URL", serviceUrl);
+        
+        RemoteResponse response = requestor.post(addServiceUrl, addServiceParameters);
+        
+        return ClientUtils.fromJson(response.getResponse(), BitbucketService.class);
+    }
 }
 
