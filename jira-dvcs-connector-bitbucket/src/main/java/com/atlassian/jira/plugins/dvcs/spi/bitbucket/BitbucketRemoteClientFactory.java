fc7f1fadb0481b9fe9f2123c4f381f098c9f7764
Martin Skurla <mskurla@atlassian.com>
2012-07-20 10:50:08 +0200

    BBC-217 - added BitbucketRemoteClientFactory

---
+++ jira-dvcs-connector-bitbucket/src/main/java/com/atlassian/jira/plugins/dvcs/spi/bitbucket/BitbucketRemoteClientFactory.java
@@ -0,0 +1,39 @@
+package com.atlassian.jira.plugins.dvcs.spi.bitbucket;
+
+import com.atlassian.jira.plugins.dvcs.model.Organization;
+import com.atlassian.jira.plugins.dvcs.model.Repository;
+import com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.client.BitbucketRemoteClient;
+import com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.request.AuthProvider;
+import com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.request.NoAuthAuthProvider;
+
+/**
+ * BitbucketRemoteClientFactory
+ *
+ * @author Martin Skurla mskurla@atlassian.com
+ */
+public final class BitbucketRemoteClientFactory
+{
+    private BitbucketRemoteClientFactory() {}
+    
+    
+    public static BitbucketRemoteClient fromOrganization(Organization organization)
+    {
+        AuthProvider authProvider = new NoAuthAuthProvider(organization.getHostUrl());
+        
+        return new BitbucketRemoteClient(authProvider);
+    }
+    
+    public static BitbucketRemoteClient fromRepository(Repository repository)
+    {
+        AuthProvider authProvider = new NoAuthAuthProvider(repository.getOrgHostUrl());
+        
+        return new BitbucketRemoteClient(authProvider);
+    }
+    
+    public static BitbucketRemoteClient fromHostUrl(String hostUrl)
+    {
+        AuthProvider authProvider = new NoAuthAuthProvider(hostUrl);
+        
+        return new BitbucketRemoteClient(authProvider);
+    }
+}
