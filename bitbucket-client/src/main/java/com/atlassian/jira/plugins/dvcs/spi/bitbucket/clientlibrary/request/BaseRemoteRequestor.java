8ca83a595e7aba003171f3700a4f9e0ebef1acff
juliushocman <devnull@localhost>
2012-07-24 13:26:03 +0200

    BBC-217 - logging

--- bitbucket-client/src/main/java/com/atlassian/jira/plugins/dvcs/spi/bitbucket/clientlibrary/request/BaseRemoteRequestor.java
+++ bitbucket-client/src/main/java/com/atlassian/jira/plugins/dvcs/spi/bitbucket/clientlibrary/request/BaseRemoteRequestor.java
@@ -2,10 +2,8 @@ package com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.request;
 
 import java.io.BufferedOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringWriter;
 import java.net.HttpURLConnection;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -30,249 +28,232 @@ import com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.client.Client
 public class BaseRemoteRequestor implements RemoteRequestor
 {
 
-	protected final String apiUrl;
-	
-	private static Logger log = LoggerFactory.getLogger(BaseRemoteRequestor.class);
+    protected final String apiUrl;
 
-	public BaseRemoteRequestor(String apiUrl)
-	{
-		super();
-		this.apiUrl = apiUrl;
-	}
+    private static Logger log = LoggerFactory.getLogger(BaseRemoteRequestor.class);
 
-	@Override
-	public RemoteResponse get(String uri, Map<String, String> parameters)
-	{
-		return requestWithoutPayload(HttpMethod.GET, uri, parameters);
-	}
-	
-	@Override
-	public RemoteResponse delete(String uri)
-	{
-		return requestWithoutPayload(HttpMethod.DELETE, uri, new HashMap<String, String>());
-	}
-
-	@Override
-	public RemoteResponse post(String uri, Map<String, String> parameters)
-	{
-		return requestWithPayload(HttpMethod.POST, uri, parameters, false);
-	}
-	
-	@Override
-	public RemoteResponse post(String uri, Object payload)
-	{
-		return requestWithPayload(HttpMethod.POST, uri, payload, true);
-	}
-
-	@Override
-	public RemoteResponse put(String uri, Map<String, String> parameters)
-	{
-		return requestWithPayload(HttpMethod.PUT, uri, parameters, false);
-	}
-	
-	@Override
-	public RemoteResponse put(String uri, Object payload)
-	{
-		return requestWithPayload(HttpMethod.PUT, uri, payload, true);
-	}
-	
-	//--------------------------------------------------------------------------------------------------
-	// extension hooks
-	//--------------------------------------------------------------------------------------------------
-	/**
-	 * E.g. append basic auth headers ... 
-	 */
-	protected void onConnectionCreated(HttpURLConnection connection, HttpMethod method) throws IOException
-	{
-
-	}
-	
-	/**
-	 * E.g. append oauth params ...
-	 */
-	protected String afterFinalUriConstructed(HttpMethod forMethod, String finalUri)
-	{
-		return finalUri;
-	}
-
-	//--------------------------------------------------------------------------------------------------
-	// Helpers
-	//--------------------------------------------------------------------------------------------------
-	
-	private RemoteResponse requestWithPayload(HttpMethod postOrPut, String uri, Object params, boolean isJson)
-	{
-		HttpURLConnection connection = null;
-		try
-		{
-			connection = createConnection(postOrPut, uri);
-			if (isJson) {
-				setPayloadOrParams(connection, params, true);
-			} else {
-				setPayloadOrParams(connection, params, false);
-			}
-			return checkAndCreateRemoteResponse(connection);
-			
-		} catch (Exception e)
-		{
-			// TODO log + message
-			throw new BitbucketRequestException("Failed to execute request " + connection, e);
-		}
-	}
-
-	private RemoteResponse requestWithoutPayload(HttpMethod getOrDelete, String uri, Map<String, String> parameters)
-	{
-		HttpURLConnection connection = null;
-		try
-		{
-			connection = createConnection(getOrDelete, uri + paramsToString(parameters, uri.contains("?")));
-			return checkAndCreateRemoteResponse(connection);
-
-		} catch (Exception e)
-		{
-			// TODO log + message
-			throw new BitbucketRequestException("Failed to execute request " + connection, e);
-		}
-	}
-	
-	private RemoteResponse checkAndCreateRemoteResponse(HttpURLConnection connection) throws IOException
-	{
-		RemoteResponse response = new RemoteResponse();
-		
-		if (connection.getResponseCode() >= 400) {
-			throw new BitbucketRequestException("Error response code during the request : " + connection.getResponseCode());
-		}
-		
-		response.setHttpStatusCode(connection.getResponseCode());
-		response.setResponse(connection.getInputStream());
-		
-		return response;
-	}
-
-	protected void logResponse(RemoteResponse response)
+    public BaseRemoteRequestor(String apiUrl)
     {
-	    log.debug("Got response with code {} and payload {} ", new Object [] { response.getHttpStatusCode(), streamToString(response.getResponse()) });
+        super();
+        this.apiUrl = apiUrl;
     }
-	
-	private String streamToString(InputStream response)
+
+    @Override
+    public RemoteResponse get(String uri, Map<String, String> parameters)
     {
-	    InputStreamReader in = new InputStreamReader(response);
-	    StringWriter out = new  StringWriter();
-	    char[] buffer = new char[4096];
-        int n = 0;
+        return requestWithoutPayload(HttpMethod.GET, uri, parameters);
+    }
+
+    @Override
+    public RemoteResponse delete(String uri)
+    {
+        return requestWithoutPayload(HttpMethod.DELETE, uri, new HashMap<String, String>());
+    }
+
+    @Override
+    public RemoteResponse post(String uri, Map<String, String> parameters)
+    {
+        return requestWithPayload(HttpMethod.POST, uri, parameters, false);
+    }
+
+    @Override
+    public RemoteResponse post(String uri, Object payload)
+    {
+        return requestWithPayload(HttpMethod.POST, uri, payload, true);
+    }
+
+    @Override
+    public RemoteResponse put(String uri, Map<String, String> parameters)
+    {
+        return requestWithPayload(HttpMethod.PUT, uri, parameters, false);
+    }
+
+    @Override
+    public RemoteResponse put(String uri, Object payload)
+    {
+        return requestWithPayload(HttpMethod.PUT, uri, payload, true);
+    }
+
+    // --------------------------------------------------------------------------------------------------
+    // extension hooks
+    // --------------------------------------------------------------------------------------------------
+    /**
+     * E.g. append basic auth headers ...
+     */
+    protected void onConnectionCreated(HttpURLConnection connection, HttpMethod method) throws IOException
+    {
+
+    }
+
+    /**
+     * E.g. append oauth params ...
+     */
+    protected String afterFinalUriConstructed(HttpMethod forMethod, String finalUri)
+    {
+        return finalUri;
+    }
+
+    // --------------------------------------------------------------------------------------------------
+    // Helpers
+    // --------------------------------------------------------------------------------------------------
+
+    private RemoteResponse requestWithPayload(HttpMethod postOrPut, String uri, Object params, boolean isJson)
+    {
+        HttpURLConnection connection = null;
         try
         {
-            while (-1 != (n = in.read(buffer))) {
-                out.write(buffer, 0, n);
+            connection = createConnection(postOrPut, uri);
+            if (isJson)
+            {
+                setPayloadOrParams(connection, params, true);
+            } else
+            {
+                setPayloadOrParams(connection, params, false);
             }
-            response.reset();
-        } catch (IOException e)
+            return checkAndCreateRemoteResponse(connection);
+
+        } catch (Exception e)
         {
-            log.warn("Failed to read response input stream.", e);
-            throw new BitbucketRequestException("Failed to read response input stream.", e);
+            // TODO log + message
+            throw new BitbucketRequestException("Failed to execute request " + connection, e);
         }
-	    return out.toString();
+    }
+
+    private RemoteResponse requestWithoutPayload(HttpMethod getOrDelete, String uri, Map<String, String> parameters)
+    {
+        HttpURLConnection connection = null;
+        try
+        {
+            connection = createConnection(getOrDelete, uri + paramsToString(parameters, uri.contains("?")));
+            return checkAndCreateRemoteResponse(connection);
+
+        } catch (Exception e)
+        {
+            // TODO log + message
+            throw new BitbucketRequestException("Failed to execute request " + connection, e);
+        }
+    }
+
+    private RemoteResponse checkAndCreateRemoteResponse(HttpURLConnection connection) throws IOException
+    {
+        RemoteResponse response = new RemoteResponse();
+
+        if (connection.getResponseCode() >= 400)
+        {
+            throw new BitbucketRequestException("Error response code during the request : "
+                    + connection.getResponseCode());
+        }
+
+        response.setHttpStatusCode(connection.getResponseCode());
+        response.setResponse(connection.getInputStream());
+
+        return response;
     }
 
     protected void logRequest(HttpURLConnection connection)
     {
-        log.debug("[{} :: {}]");
+        log.debug("[{} :: {}]", new Object[] { connection.getRequestMethod(), connection.getURL() });
     }
 
     protected String paramsToString(Map<String, String> parameters, boolean urlAlreadyHasParams)
-	{
-		StringBuilder queryStringBuilder = new StringBuilder();
+    {
+        StringBuilder queryStringBuilder = new StringBuilder();
 
-		if (parameters != null && !parameters.isEmpty())
-		{
-			if (!urlAlreadyHasParams)
-			{
-				queryStringBuilder.append("?");
-			} else {
-				queryStringBuilder.append("&");
-			}
-			for (Iterator<Map.Entry<String, String>> iterator = parameters.entrySet().iterator(); iterator.hasNext();)
-			{
-				Map.Entry<String, String> entry = iterator.next();
-				queryStringBuilder.append(encode(entry.getKey()));
-				queryStringBuilder.append("=");
-				queryStringBuilder.append(encode(entry.getValue()));
-				if (iterator.hasNext())
+        if (parameters != null && !parameters.isEmpty())
+        {
+            if (!urlAlreadyHasParams)
+            {
+                queryStringBuilder.append("?");
+            } else
+            {
+                queryStringBuilder.append("&");
+            }
+            for (Iterator<Map.Entry<String, String>> iterator = parameters.entrySet().iterator(); iterator.hasNext();)
+            {
+                Map.Entry<String, String> entry = iterator.next();
+                queryStringBuilder.append(encode(entry.getKey()));
+                queryStringBuilder.append("=");
+                queryStringBuilder.append(encode(entry.getValue()));
+                if (iterator.hasNext())
                 {
-					queryStringBuilder.append("&");
+                    queryStringBuilder.append("&");
                 }
-			}
-		}
-		return queryStringBuilder.toString();
-	}
+            }
+        }
+        return queryStringBuilder.toString();
+    }
 
-	private static String encode(String str)
-	{
-		if (str == null)
-		{
-			return null;
-		}
+    private static String encode(String str)
+    {
+        if (str == null)
+        {
+            return null;
+        }
 
-		try
-		{
-			// return URLEncoder.encode(str, "UTF-8");
-		    return str;
-		} catch (Exception e)
-		{
-			throw new BitbucketRequestException("Required encoding not found", e);
-		}
-	}
+        try
+        {
+            return URLEncoder.encode(str, "UTF-8");
+        } catch (Exception e)
+        {
+            throw new BitbucketRequestException("Required encoding not found", e);
+        }
+    }
 
-	private HttpURLConnection createConnection(HttpMethod method, String uri) throws IOException
-	{
-		HttpURLConnection connection = method.createConnection(afterFinalUriConstructed(method, apiUrl + uri));
-		//
-		// something to extend
-		//
-		onConnectionCreated(connection, method);
+    private HttpURLConnection createConnection(HttpMethod method, String uri) throws IOException
+    {
+        HttpURLConnection connection = method.createConnection(afterFinalUriConstructed(method, apiUrl + uri));
+        //
+        logRequest(connection);
+        //
+        //
+        // something to extend
+        //
+        onConnectionCreated(connection, method);
 
-		return connection;
-	}
+        return connection;
+    }
 
-	@SuppressWarnings("all")
-	private void setPayloadOrParams(HttpURLConnection connection, Object params, boolean isJson) throws IOException
-	{
-		connection.setDoOutput(true);
-		
-		if (params != null)
-		{
-			byte[] data = new byte [] {};
+    @SuppressWarnings("all")
+    private void setPayloadOrParams(HttpURLConnection connection, Object params, boolean isJson) throws IOException
+    {
+        connection.setDoOutput(true);
 
-			if (isJson) {
-				connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-				data = ClientUtils.toJson(params).getBytes("UTF-8");
-			} else {
-				// assuming post/put kind of "form" params
-				data = paramsToString((Map) params, true).getBytes("UTF-8");
-			}
+        if (params != null)
+        {
+            byte[] data = new byte[] {};
 
-			connection.setFixedLengthStreamingMode(data.length);
-			BufferedOutputStream output = new BufferedOutputStream(connection.getOutputStream());
-			try
-			{
+            if (isJson)
+            {
+                connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                data = ClientUtils.toJson(params).getBytes("UTF-8");
+            } else
+            {
+                // assuming post/put kind of "form" params
+                data = paramsToString((Map) params, true).getBytes("UTF-8");
+            }
 
-				output.write(data);
-				output.flush();
-			
-			} finally
-			{
-				try
-				{
-					output.close();
-				} catch (IOException ignored)
-				{
-					// nop
-				}
-			}
-		} else
-		{
-			connection.setFixedLengthStreamingMode(0);
-		}
-	}
-	
+            connection.setFixedLengthStreamingMode(data.length);
+            BufferedOutputStream output = new BufferedOutputStream(connection.getOutputStream());
+            try
+            {
+
+                output.write(data);
+                output.flush();
+
+            } finally
+            {
+                try
+                {
+                    output.close();
+                } catch (IOException ignored)
+                {
+                    // nop
+                }
+            }
+        } else
+        {
+            connection.setFixedLengthStreamingMode(0);
+        }
+    }
 
 }
