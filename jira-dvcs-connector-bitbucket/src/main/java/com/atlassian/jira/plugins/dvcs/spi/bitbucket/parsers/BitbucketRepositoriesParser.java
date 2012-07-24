2aba39ee30cca827b7deed7e6276812dd73c29ac
Martin Skurla <mskurla@atlassian.com>
2012-07-24 14:43:04 +0200

    BBC-217 - unused classes are commented

--- jira-dvcs-connector-bitbucket/src/main/java/com/atlassian/jira/plugins/dvcs/spi/bitbucket/parsers/BitbucketRepositoriesParser.java
+++ jira-dvcs-connector-bitbucket/src/main/java/com/atlassian/jira/plugins/dvcs/spi/bitbucket/parsers/BitbucketRepositoriesParser.java
@@ -1,30 +1,30 @@
-package com.atlassian.jira.plugins.dvcs.spi.bitbucket.parsers;
-
-import com.atlassian.jira.plugins.dvcs.model.Repository;
-import com.atlassian.jira.util.json.JSONArray;
-import com.atlassian.jira.util.json.JSONException;
-import com.atlassian.jira.util.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class BitbucketRepositoriesParser
-{
-
-    public static List<Repository> parseRepositoryNames(JSONObject jsonObject) throws JSONException
-    {
-        List<Repository> repositories = new ArrayList<Repository>();
-        JSONArray jsonArray = jsonObject.getJSONArray("repositories");
-        for (int i = 0; i < jsonArray.length(); i++)
-        {
-            JSONObject repositoryJson  = (JSONObject) jsonArray.get(i);
-
-            Repository repository = new Repository();
-            repository.setSlug(repositoryJson.getString("slug"));
-            repository.setName(repositoryJson.getString("name"));
-            repositories.add(repository);
-        }
-        return repositories;
-    }
-
-}
+//package com.atlassian.jira.plugins.dvcs.spi.bitbucket.parsers;
+//
+//import com.atlassian.jira.plugins.dvcs.model.Repository;
+//import com.atlassian.jira.util.json.JSONArray;
+//import com.atlassian.jira.util.json.JSONException;
+//import com.atlassian.jira.util.json.JSONObject;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class BitbucketRepositoriesParser
+//{
+//
+//    public static List<Repository> parseRepositoryNames(JSONObject jsonObject) throws JSONException
+//    {
+//        List<Repository> repositories = new ArrayList<Repository>();
+//        JSONArray jsonArray = jsonObject.getJSONArray("repositories");
+//        for (int i = 0; i < jsonArray.length(); i++)
+//        {
+//            JSONObject repositoryJson  = (JSONObject) jsonArray.get(i);
+//
+//            Repository repository = new Repository();
+//            repository.setSlug(repositoryJson.getString("slug"));
+//            repository.setName(repositoryJson.getString("name"));
+//            repositories.add(repository);
+//        }
+//        return repositories;
+//    }
+//
+//}
--- jira-dvcs-connector-bitbucket/src/main/java/com/atlassian/jira/plugins/dvcs/spi/bitbucket/parsers/BitbucketUserFactory.java
+++ jira-dvcs-connector-bitbucket/src/main/java/com/atlassian/jira/plugins/dvcs/spi/bitbucket/parsers/BitbucketUserFactory.java
@@ -1,28 +1,28 @@
-package com.atlassian.jira.plugins.dvcs.spi.bitbucket.parsers;
-
-import com.atlassian.jira.plugins.dvcs.exception.SourceControlException;
-import com.atlassian.jira.plugins.dvcs.model.DvcsUser;
-import com.atlassian.jira.util.json.JSONException;
-import com.atlassian.jira.util.json.JSONObject;
-
-public class BitbucketUserFactory
-{
-
-	public static DvcsUser parse(JSONObject userJson)
-	{
-	    try
-	    {
-	        return new DvcsUser(
-	                userJson.getString("username"),
-	                userJson.getString("first_name"),
-	                userJson.getString("last_name"),
-	                userJson.getString("avatar")
-	        );
-	    }
-	    catch (JSONException e)
-	    {
-	        throw new SourceControlException("invalid json object", e);
-	    }
-	}
-
-}
+//package com.atlassian.jira.plugins.dvcs.spi.bitbucket.parsers;
+//
+//import com.atlassian.jira.plugins.dvcs.exception.SourceControlException;
+//import com.atlassian.jira.plugins.dvcs.model.DvcsUser;
+//import com.atlassian.jira.util.json.JSONException;
+//import com.atlassian.jira.util.json.JSONObject;
+//
+//public class BitbucketUserFactory
+//{
+//
+//	public static DvcsUser parse(JSONObject userJson)
+//	{
+//	    try
+//	    {
+//	        return new DvcsUser(
+//	                userJson.getString("username"),
+//	                userJson.getString("first_name"),
+//	                userJson.getString("last_name"),
+//	                userJson.getString("avatar")
+//	        );
+//	    }
+//	    catch (JSONException e)
+//	    {
+//	        throw new SourceControlException("invalid json object", e);
+//	    }
+//	}
+//
+//}
