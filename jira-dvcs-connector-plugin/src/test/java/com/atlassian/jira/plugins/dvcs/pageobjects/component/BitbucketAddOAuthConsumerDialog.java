1e1363f9a988527bebefd8005fc4a2da5e87dd4a
Martin Skurla <mskurla@atlassian.com>
2012-08-23 14:32:46 +0200

    Transplant n. 2 of BBC-232 - Bitbucket integration tests will now generate and use OAuth credentials for every test method

---
+++ jira-dvcs-connector-plugin/src/test/java/com/atlassian/jira/plugins/dvcs/pageobjects/component/BitbucketAddOAuthConsumerDialog.java
@@ -0,0 +1,37 @@
+package com.atlassian.jira.plugins.dvcs.pageobjects.component;
+
+import org.openqa.selenium.By;
+import com.atlassian.jira.plugins.dvcs.util.PageElementUtils;
+import com.atlassian.pageobjects.elements.PageElement;
+
+/**
+ * @author Martin Skurla mskurla@atlassian.com
+ */
+public class BitbucketAddOAuthConsumerDialog
+{      
+    private final PageElement addOAuthConsumerDialog;
+    
+
+    public BitbucketAddOAuthConsumerDialog(PageElement addOAuthConsumerDialog)
+    {
+        this.addOAuthConsumerDialog = addOAuthConsumerDialog;
+    }
+
+
+    public void addConsumer(String consumerName, String consumerDescription)
+    {       
+        PageElement consumerDescriptionInput = addOAuthConsumerDialog.find(By.id("consumer-description"));
+
+        // for some unknown reason this must be set before consumer name, otherwise consumer name will not be set at all
+        consumerDescriptionInput.type(consumerDescription);
+       
+        PageElement consumerNameInput = addOAuthConsumerDialog.find(By.id("consumer-name"));        
+        consumerNameInput.click().type(consumerName);
+
+        PageElement addConsumerButton = PageElementUtils.findTagWithAttribute(addOAuthConsumerDialog,
+                                                                              "input",
+                                                                              "type",
+                                                                              "submit");
+        addConsumerButton.click();
+    }
+}
