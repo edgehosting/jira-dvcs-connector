21472042c68a413dc843f872242419a5b40c670c
juliushocman <devnull@localhost>
2012-09-07 17:18:40 +0200

    transplant 12a91f820239 from default

--- jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/service/OrganizationServiceImpl.java
+++ jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/service/OrganizationServiceImpl.java
@@ -106,7 +106,7 @@ public class OrganizationServiceImpl implements OrganizationService
 		org = organizationDao.save(organization);
 
 		// sync repository list
-		repositoryService.syncRepositoryList(org);
+		repositoryService.syncRepositoryList(org, false);
 
 		return org;
 	}
