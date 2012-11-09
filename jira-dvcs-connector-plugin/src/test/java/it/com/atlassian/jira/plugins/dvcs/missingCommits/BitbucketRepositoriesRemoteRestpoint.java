3b677d23997e87e9c8dea22dbe844ba8322c69c0
Martin Skurla <mskurla@atlassian.com>
2012-11-09 10:26:47 +0100

    BBC-318 - Added REST point to create/remove BitBucket Hg repositories and Zip extraction utility class

---
+++ jira-dvcs-connector-plugin/src/test/java/it/com/atlassian/jira/plugins/dvcs/missingCommits/BitbucketRepositoriesRemoteRestpoint.java
@@ -0,0 +1,44 @@
+package it.com.atlassian.jira.plugins.dvcs.missingCommits;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.request.RemoteRequestor;
+
+/**
+ * @author Martin Skurla
+ */
+public class BitbucketRepositoriesRemoteRestpoint
+{
+	private final RemoteRequestor requestor;
+
+	public BitbucketRepositoriesRemoteRestpoint(RemoteRequestor requestor)
+	{
+		this.requestor = requestor;
+	}
+
+    public void createHgRepository(String repositoryName)
+    {
+        createRepository(repositoryName, "hg");
+    }
+
+	private void createRepository(String repositoryName, String scm)
+    {
+        Map<String, String> createRepoPostData = new HashMap<String, String>();
+        createRepoPostData.put("name", repositoryName);
+        createRepoPostData.put("scm",  scm);
+
+		requestor.post("/repositories", createRepoPostData);
+	}
+
+    public void removeExistingRepository(String repositoryName, String owner)
+    {
+        Map<String, String> removeRepoPostData = new HashMap<String, String>();
+        removeRepoPostData.put("repo_slug",   repositoryName);
+        removeRepoPostData.put("accountname", owner);
+
+        String removeRepositoryUrl = String.format("/repositories/%s/%s", owner, repositoryName);
+
+        requestor.delete(removeRepositoryUrl, removeRepoPostData);
+    }
+}
