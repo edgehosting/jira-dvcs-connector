090604b70817bd998f0f1c202fff1fceafd4c4b2
Miroslav Stencel <mstencel@atlassian.com>
2012-11-19 11:56:48 +0100

    BBC-334 integration test for verifying missing commits for Github

---
+++ jira-dvcs-connector-plugin/src/test/java/it/com/atlassian/jira/plugins/dvcs/missingCommits/GithubRepositoriesRemoteRestpoint.java
@@ -0,0 +1,60 @@
+package it.com.atlassian.jira.plugins.dvcs.missingCommits;
+
+import java.io.IOException;
+
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.RepositoryService;
+
+/**
+ * @author Miroslav Stencel
+ */
+public class GithubRepositoriesRemoteRestpoint
+{
+    private final GitHubClient gitHubClient;
+    private RepositoryService repositoryService;
+    
+    public GithubRepositoriesRemoteRestpoint(GitHubClient gitHubClient)
+    {
+        this.gitHubClient = gitHubClient;
+    }
+
+    public void createGithubRepository(String repositoryName)
+    {
+        Repository repository = new Repository();
+        repository.setName(repositoryName);
+        
+        try
+        {
+            getRepositoryService().createRepository(repository);
+        } catch (IOException e)
+        {
+            // repository could not be created
+        }
+    }
+
+    public void removeExistingRepository(String repositoryName, String owner)
+    {
+        StringBuilder deleteRepoUri = new StringBuilder("/repos/");
+        deleteRepoUri.append(owner).append("/").append(repositoryName);
+        
+        try
+        {
+            gitHubClient.delete(deleteRepoUri.toString());
+        }
+        catch (IOException e)
+        {
+            // status is not a 204 (No Content).
+        }
+    }
+    
+    private RepositoryService getRepositoryService()
+    {
+        if (repositoryService == null)
+        {
+            repositoryService = new RepositoryService(gitHubClient);
+        }
+        
+        return repositoryService;
+    }
+}
