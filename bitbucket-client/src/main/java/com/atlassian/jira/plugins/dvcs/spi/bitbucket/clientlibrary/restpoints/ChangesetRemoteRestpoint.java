89ab48c7470b7df66f102b5da346d6b5b7b35b9e
Dusan Hornik <dusan.hornik@hotovo.org>
2013-01-11 15:57:27 +0100

    BBC-386 adding retry + backoff logic when requesting commit statistics

--- bitbucket-client/src/main/java/com/atlassian/jira/plugins/dvcs/spi/bitbucket/clientlibrary/restpoints/ChangesetRemoteRestpoint.java
+++ bitbucket-client/src/main/java/com/atlassian/jira/plugins/dvcs/spi/bitbucket/clientlibrary/restpoints/ChangesetRemoteRestpoint.java
@@ -19,8 +19,6 @@ import com.google.gson.reflect.TypeToken;
  */
 public class ChangesetRemoteRestpoint
 {
-    private static final int DIFFSTAT_NO_LIMIT = -1;
-
     private final RemoteRequestor requestor;
 
     public ChangesetRemoteRestpoint(RemoteRequestor remoteRequestor)
@@ -43,20 +41,13 @@ public class ChangesetRemoteRestpoint
         });
     }
 
-    public List<BitbucketChangesetWithDiffstat> getChangesetDiffStat(String owner, String slug, String node)
-    {
-        return getChangesetDiffStat(owner, slug, node, DIFFSTAT_NO_LIMIT);
-    }
 
     public List<BitbucketChangesetWithDiffstat> getChangesetDiffStat(String owner, String slug, String node, int limit)
     {
         String getChangesetDiffStatUrl = URLPathFormatter.format("/repositories/%s/%s/changesets/%s/diffstat", owner, slug, node);
 
         Map<String, String> parameters = null;
-        if (limit != DIFFSTAT_NO_LIMIT)
-        {
-            parameters = Collections.singletonMap("limit", "" + limit);
-        }
+        parameters = Collections.singletonMap("limit", "" + limit);
 
         return requestor.get(getChangesetDiffStatUrl, parameters,
                 new ResponseCallback<List<BitbucketChangesetWithDiffstat>>()
