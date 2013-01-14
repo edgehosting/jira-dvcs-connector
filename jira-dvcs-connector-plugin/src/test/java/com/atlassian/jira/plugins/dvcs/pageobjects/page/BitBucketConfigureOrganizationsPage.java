30e419fcdb070303398b8ba686cb3b550e312a8b
Dusan Hornik <dusan.hornik@hotovo.org>
2013-01-15 00:47:08 +0100

    BBC-386 Increasing wait time for incorrect url

--- jira-dvcs-connector-plugin/src/test/java/com/atlassian/jira/plugins/dvcs/pageobjects/page/BitBucketConfigureOrganizationsPage.java
+++ jira-dvcs-connector-plugin/src/test/java/com/atlassian/jira/plugins/dvcs/pageobjects/page/BitBucketConfigureOrganizationsPage.java
@@ -1,11 +1,14 @@
 package com.atlassian.jira.plugins.dvcs.pageobjects.page;
 
+import static org.hamcrest.Matchers.is;
+
 import org.openqa.selenium.By;
 
 import com.atlassian.pageobjects.elements.ElementBy;
 import com.atlassian.pageobjects.elements.PageElement;
 import com.atlassian.pageobjects.elements.query.Conditions;
 import com.atlassian.pageobjects.elements.query.Poller;
+import com.atlassian.pageobjects.elements.query.TimedCondition;
 
 /**
  * Represents the page to link repositories to projects.
@@ -68,7 +71,9 @@ public class BitBucketConfigureOrganizationsPage extends BaseConfigureOrganizati
 
         addOrgButton.click();
 
-        Poller.waitUntilTrue("Expected Error message while connecting repository", messageBarDiv.find(By.tagName("strong")).timed().hasText("Error!"));
+        TimedCondition hasText = messageBarDiv.find(By.tagName("strong")).timed().hasText("Error!");
+        
+        Poller.waitUntil("Expected Error message while connecting repository", hasText, is(true), Poller.by(30000));
 
         return this;
     }
