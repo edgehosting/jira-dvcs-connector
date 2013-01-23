f5136c209343cd00b14afeff2684233b03a60d49
juliushocman <devnull@localhost>
2013-01-23 16:41:05 +0100

    BBC-388 some model extensions

--- bitbucket-client/src/main/java/com/atlassian/jira/plugins/dvcs/spi/bitbucket/clientlibrary/model/BitbucketPullRequestUpdateActivity.java
+++ bitbucket-client/src/main/java/com/atlassian/jira/plugins/dvcs/spi/bitbucket/clientlibrary/model/BitbucketPullRequestUpdateActivity.java
@@ -3,8 +3,20 @@ package com.atlassian.jira.plugins.dvcs.spi.bitbucket.clientlibrary.model;
 import java.io.Serializable;
 
 /**
- * BitbucketPullRequest
- *
+ * TODO commit on update ?? not array ?
+ * 
+ *          "source": {
+                    "commit": {
+                        "sha": "4a233e7b8596",
+                        "href": "/api/1.0/repositories/erik/bitbucket/changesets/4a233e7b8596"
+                    },
+                    "branch": {
+                        "name": "erik/captcha"
+                    },
+                    "repository": {
+                        "href": "/api/1.0/repositories/erik/bitbucket"
+                    }
+                },
  * 
  * <br /><br />
  * Created on 11.12.2012, 14:02:57
@@ -15,11 +27,36 @@ import java.io.Serializable;
 public class BitbucketPullRequestUpdateActivity extends BitbucketPullRequestBaseActivity implements Serializable, HasPossibleUpdatedMessages
 {
     private static final long serialVersionUID = -7371697488007134175L;
+    
+    private String title;
+    
+    private String description;
 
     public BitbucketPullRequestUpdateActivity()
     {
         super();
     }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+    
 
 }
 
