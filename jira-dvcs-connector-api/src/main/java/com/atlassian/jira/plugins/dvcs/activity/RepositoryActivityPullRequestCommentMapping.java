7715ba8c157f40ce5f0c76d0260590dcab5f7f8b
Miroslav Stencel <mstencel@atlassian.com>
2013-02-07 15:54:49 +0100

    BBC-400 Updating Pull Request Comment mapping

--- jira-dvcs-connector-api/src/main/java/com/atlassian/jira/plugins/dvcs/activity/RepositoryActivityPullRequestCommentMapping.java
+++ jira-dvcs-connector-api/src/main/java/com/atlassian/jira/plugins/dvcs/activity/RepositoryActivityPullRequestCommentMapping.java
@@ -1,13 +1,28 @@
 package com.atlassian.jira.plugins.dvcs.activity;
 
 import net.java.ao.Preload;
+import net.java.ao.schema.StringLength;
 import net.java.ao.schema.Table;
 
 @Preload
 @Table("PrComment")
-public interface RepositoryActivityPullRequestCommentMapping extends RepositoryActivityPullRequestMapping {
-    
-  
-    
+public interface RepositoryActivityPullRequestCommentMapping extends RepositoryActivityPullRequestMapping
+{
+	String COMMENT_URL = "COMMENT_URL";
+	String MESSAGE = "MESSAGE";
+
+	//
+    // getters
+    //
+    String getCommentUrl();
+	@StringLength(StringLength.UNLIMITED)
+    String getMessage();
+	
+    //
+    // setters
+    //
+    void setCommentUrl(String url);
+    @StringLength(StringLength.UNLIMITED)
+    void setMessage(String message);
 }
 
