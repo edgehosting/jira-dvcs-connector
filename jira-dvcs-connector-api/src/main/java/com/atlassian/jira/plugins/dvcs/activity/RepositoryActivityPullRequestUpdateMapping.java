5fb93d9acad30f811f79895d1ca28ef2df70d2f3
Miroslav Stencel <mstencel@atlassian.com>
2013-02-12 13:30:52 +0100

    BBC-400 Adding commits to update activity mapping

--- jira-dvcs-connector-api/src/main/java/com/atlassian/jira/plugins/dvcs/activity/RepositoryActivityPullRequestUpdateMapping.java
+++ jira-dvcs-connector-api/src/main/java/com/atlassian/jira/plugins/dvcs/activity/RepositoryActivityPullRequestUpdateMapping.java
@@ -1,13 +1,16 @@
 package com.atlassian.jira.plugins.dvcs.activity;
 
+import net.java.ao.OneToMany;
 import net.java.ao.schema.Table;
 
-@Table("PrUpdate")
+@Table("PR_UPDATE")
 public interface RepositoryActivityPullRequestUpdateMapping extends RepositoryActivityPullRequestMapping {
     
     String STATUS = "STATUS";
     
     String getStatus();
+    @OneToMany
+    RepositoryActivityCommitMapping[] getCommits();
     
     void setStatus(String status);
 }
