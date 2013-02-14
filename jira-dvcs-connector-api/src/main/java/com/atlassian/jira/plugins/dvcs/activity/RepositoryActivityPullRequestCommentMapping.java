79d2f5d353a2ba313bfc7ee5a9aa89cf3b52a082
Miroslav Stencel <mstencel@atlassian.com>
2013-02-14 15:47:43 +0100

    BBC-400 Adding parent of the comment

--- jira-dvcs-connector-api/src/main/java/com/atlassian/jira/plugins/dvcs/activity/RepositoryActivityPullRequestCommentMapping.java
+++ jira-dvcs-connector-api/src/main/java/com/atlassian/jira/plugins/dvcs/activity/RepositoryActivityPullRequestCommentMapping.java
@@ -9,17 +9,20 @@ import net.java.ao.schema.Table;
 public interface RepositoryActivityPullRequestCommentMapping extends RepositoryActivityPullRequestMapping
 {
     String MESSAGE = "MESSAGE";
-
+    String PARENT_ID = "PARENT_ID";
+    
     //
     // getters
     //
     @StringLength(StringLength.UNLIMITED)
     String getMessage();
+    int getParentId();
     
     //
     // setters
     //
     @StringLength(StringLength.UNLIMITED)
     void setMessage(String message);
+    void setParentId(int parentId);
 }
 
