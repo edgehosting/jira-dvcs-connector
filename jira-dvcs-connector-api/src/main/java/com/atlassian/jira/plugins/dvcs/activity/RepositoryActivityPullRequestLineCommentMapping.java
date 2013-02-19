d5da14f057fe053672ac62a7a6004ca8814e327c
Miroslav Stencel <mstencel@atlassian.com>
2013-02-19 11:06:44 +0100

    BBC-399 Refactoring comment mappings, removing not null constraint on commentUrl

--- jira-dvcs-connector-api/src/main/java/com/atlassian/jira/plugins/dvcs/activity/RepositoryActivityPullRequestLineCommentMapping.java
+++ jira-dvcs-connector-api/src/main/java/com/atlassian/jira/plugins/dvcs/activity/RepositoryActivityPullRequestLineCommentMapping.java
@@ -1,37 +1,23 @@
 package com.atlassian.jira.plugins.dvcs.activity;
 
 import net.java.ao.Preload;
-import net.java.ao.schema.NotNull;
-import net.java.ao.schema.StringLength;
 import net.java.ao.schema.Table;
 
 @Preload
 @Table("PR_LINE_COMMENT")
-public interface RepositoryActivityPullRequestLineCommentMapping extends RepositoryActivityPullRequestMapping
+public interface RepositoryActivityPullRequestLineCommentMapping extends RepositoryActivityPullRequestCommentMapping
 {
-    String COMMENT_URL = "COMMENT_URL";
-    String MESSAGE = "MESSAGE";
     String FILE = "FILE";
 
     //
     // getters
     //
-    @NotNull
-    String getCommentUrl();
 
-    @StringLength(StringLength.UNLIMITED)
-    String getMessage();
-
-    @StringLength(StringLength.UNLIMITED)
     String getFile();
     
     //
     // setters
     //
     
-    void setCommentUrl(String commentUrl);
-
-    void setMessage(String message);
-    
-    void setPath(String file);
+    void setFile(String file);
 }
