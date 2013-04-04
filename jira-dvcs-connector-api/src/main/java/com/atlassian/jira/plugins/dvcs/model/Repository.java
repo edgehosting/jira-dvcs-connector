6dd6f01e7f7febaea58f34251404f869fb7e1b5f
Miroslav Stencel <mstencel@atlassian.com>
2013-04-04 13:47:43 +0200

    BBC-473 Add visual information that there is no administration permissions for adding post commit hook

--- jira-dvcs-connector-api/src/main/java/com/atlassian/jira/plugins/dvcs/model/Repository.java
+++ jira-dvcs-connector-api/src/main/java/com/atlassian/jira/plugins/dvcs/model/Repository.java
@@ -30,6 +30,7 @@ public class Repository
 	private transient Credential credential;
 	private transient String orgHostUrl;
 	private transient String orgName;
+	private transient boolean adminPermission;
 	
 	@XmlElement
     private DefaultProgress sync;
@@ -172,6 +173,16 @@ public class Repository
 	{
 		this.sync = sync;
 	}
+
+    public boolean hasAdminPermission()
+    {
+        return adminPermission;
+    }
+
+    public void setAdminPermission(boolean adminPermission)
+    {
+        this.adminPermission = adminPermission;
+    }
 
     @Override
 	public boolean equals(Object obj)
