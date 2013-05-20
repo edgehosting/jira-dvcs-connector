2db7f300b8a54fdb29b4c3de2a5a3d9a6e8520c3
Stanislav Dvorscak <sdvorscak@atlassian.com>
2013-05-20 22:35:04 +0200

    BBC-529: Migrated default groups fragment from REST to soy template

--- jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/webfragments/WebfragmentRenderer.java
+++ jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/webfragments/WebfragmentRenderer.java
@@ -4,15 +4,6 @@ import java.io.IOException;
 
 public interface WebfragmentRenderer
 {
-
-    /**
-     * Renders list of groups for default groups configuration for DVCS account 
-     * 
-     * @param orgId organization id
-     * @return 
-     * @throws IOException
-     */
-	String renderDefaultGroupsFragment(int orgId) throws IOException;
 	
 	/**
 	 * Renders fragment with list of groups with checkboxes for add user dialog
