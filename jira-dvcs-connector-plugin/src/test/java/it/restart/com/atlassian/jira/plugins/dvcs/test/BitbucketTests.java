3a89d0d59c1e1ec6bf10ff692320561be19db5bd
Dusan Hornik <dusan.hornik@hotovo.org>
2013-05-25 01:15:29 +0200

    Cleaning up postcommit hooks on github before testing hooks

--- jira-dvcs-connector-plugin/src/test/java/it/restart/com/atlassian/jira/plugins/dvcs/test/BitbucketTests.java
+++ jira-dvcs-connector-plugin/src/test/java/it/restart/com/atlassian/jira/plugins/dvcs/test/BitbucketTests.java
@@ -13,16 +13,8 @@ import it.restart.com.atlassian.jira.plugins.dvcs.bitbucket.BitbucketOAuthPage;
 import it.restart.com.atlassian.jira.plugins.dvcs.common.MagicVisitor;
 import it.restart.com.atlassian.jira.plugins.dvcs.common.OAuth;
 
-import java.io.IOException;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.UsernamePasswordCredentials;
-import org.apache.commons.httpclient.auth.AuthScope;
-import org.apache.commons.httpclient.methods.DeleteMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -36,6 +28,7 @@ import com.atlassian.jira.pageobjects.pages.DashboardPage;
 import com.atlassian.jira.plugins.dvcs.pageobjects.component.BitBucketCommitEntry;
 import com.atlassian.jira.plugins.dvcs.pageobjects.page.JiraViewIssuePage;
 import com.atlassian.jira.plugins.dvcs.pageobjects.page.OAuthCredentials;
+import com.atlassian.jira.plugins.dvcs.util.HttpSenderUtils;
 import com.atlassian.jira.plugins.dvcs.util.PasswordUtil;
 import com.atlassian.pageobjects.TestedProductFactory;
 import com.atlassian.pageobjects.elements.PageElement;
@@ -46,11 +39,11 @@ public class BitbucketTests implements BasicTests, MissingCommitsTests, Activity
     private static final String ACCOUNT_NAME = "jirabitbucketconnector";
     private OAuth oAuth;
 
-    
+
     @BeforeClass
     public void beforeClass()
     {
-        // log in to JIRA 
+        // log in to JIRA
         new JiraLoginPageController(jira).login();
         // log in to Bitbucket
         new MagicVisitor(jira).visit(BitbucketLoginPage.class).doLogin();
@@ -58,7 +51,7 @@ public class BitbucketTests implements BasicTests, MissingCommitsTests, Activity
         oAuth = new MagicVisitor(jira).visit(BitbucketOAuthPage.class).addConsumer();
 //        jira.visit(JiraBitbucketOAuthPage.class).setCredentials(oAuth.key, oAuth.secret);
     }
-    
+
     @AfterClass
     public void afterClass()
     {
@@ -70,14 +63,13 @@ public class BitbucketTests implements BasicTests, MissingCommitsTests, Activity
         // log out from bitbucket
         new MagicVisitor(jira).visit(BitbucketLoginPage.class).doLogout();
     }
-    
+
     @BeforeMethod
     public void beforeMethod()
     {
         RepositoriesPageController rpc = new RepositoriesPageController(jira);
         rpc.getPage().deleteAllOrganizations();
     }
-
 
     @Override
     @Test
@@ -86,29 +78,29 @@ public class BitbucketTests implements BasicTests, MissingCommitsTests, Activity
         RepositoriesPageController rpc = new RepositoriesPageController(jira);
         OrganizationDiv organization = rpc.addOrganization(AccountType.BITBUCKET, ACCOUNT_NAME, getOAuthCredentials(), false);
 
-        assertThat(organization).isNotNull(); 
-        assertThat(organization.getRepositories().size()).isEqualTo(4);  
-        
+        assertThat(organization).isNotNull();
+        assertThat(organization.getRepositories().size()).isEqualTo(4);
+
         // check add user extension
         PageElement dvcsExtensionsPanel = jira.visit(JiraAddUserPage.class).getDvcsExtensionsPanel();
         assertThat(dvcsExtensionsPanel.isVisible());
     }
-    
+
     @Override
     @Test
     public void addOrganizationWaitForSync()
     {
         RepositoriesPageController rpc = new RepositoriesPageController(jira);
         OrganizationDiv organization = rpc.addOrganization(AccountType.BITBUCKET, ACCOUNT_NAME, getOAuthCredentials(), true);
-        
-        assertThat(organization).isNotNull(); 
+
+        assertThat(organization).isNotNull();
         assertThat(organization.getRepositories().size()).isEqualTo(4);
         assertThat(organization.getRepositories().get(3).getMessage()).isEqualTo("Fri Mar 02 2012");
-        
+
         assertThat(getCommitsForIssue("QA-2", 1)).hasItemWithCommitMessage("BB modified 1 file to QA-2 and QA-3 from TestRepo-QA");
         assertThat(getCommitsForIssue("QA-3", 2)).hasItemWithCommitMessage("BB modified 1 file to QA-2 and QA-3 from TestRepo-QA");
     }
-    
+
     @Override
     @Test(expectedExceptions = AssertionError.class, expectedExceptionsMessageRegExp = ".*Error!\\nThe url \\[https://privatebitbucket.org\\] is incorrect or the server is not responding.*")
     public void addOrganizationInvalidUrl()
@@ -116,7 +108,7 @@ public class BitbucketTests implements BasicTests, MissingCommitsTests, Activity
         RepositoriesPageController rpc = new RepositoriesPageController(jira);
         rpc.addOrganization(AccountType.BITBUCKET, "https://privatebitbucket.org/someaccount", getOAuthCredentials(), false);
     }
-    
+
     @Override
     @Test(expectedExceptions = AssertionError.class, expectedExceptionsMessageRegExp = ".*Error!\\nInvalid user/team account.*")
     public void addOrganizationInvalidAccount()
@@ -131,9 +123,9 @@ public class BitbucketTests implements BasicTests, MissingCommitsTests, Activity
     {
         RepositoriesPageController rpc = new RepositoriesPageController(jira);
         OrganizationDiv organization = rpc.addOrganization(AccountType.BITBUCKET, ACCOUNT_NAME, new OAuthCredentials("bad", "credentials"), true);
-        
-        assertThat(organization).isNotNull(); 
-        assertThat(organization.getRepositories().size()).isEqualTo(4);  
+
+        assertThat(organization).isNotNull();
+        assertThat(organization.getRepositories().size()).isEqualTo(4);
     }
 
     @Test
@@ -173,14 +165,15 @@ public class BitbucketTests implements BasicTests, MissingCommitsTests, Activity
     {
         // remove existing
         String bitbucketServiceConfigUrl = "https://bitbucket.org/!api/1.0/repositories/jirabitbucketconnector/public-hg-repo/services";
-        removeExistingServices(bitbucketServiceConfigUrl, "jirabitbucketconnector", PasswordUtil.getPassword("jirabitbucketconnector"));
+        HttpSenderUtils.removeJsonElementsUsingIDs(bitbucketServiceConfigUrl, "jirabitbucketconnector", PasswordUtil.getPassword("jirabitbucketconnector"));
 
         // add organization
         RepositoriesPageController rpc = new RepositoriesPageController(jira);
         rpc.addOrganization(AccountType.BITBUCKET, ACCOUNT_NAME, getOAuthCredentials(), true);
 
         // check postcommit hook is there
-        String servicesConfig = getBitbucketServices(bitbucketServiceConfigUrl, "jirabitbucketconnector", PasswordUtil.getPassword("jirabitbucketconnector"));
+        String servicesConfig = HttpSenderUtils.makeHttpRequest(new GetMethod(bitbucketServiceConfigUrl),
+                "jirabitbucketconnector", PasswordUtil.getPassword("jirabitbucketconnector"));
         String baseUrl = jira.getProductInstance().getBaseUrl();
         String syncUrl = baseUrl + "/rest/bitbucket/1.0/repository/";
         assertThat(servicesConfig).contains(syncUrl);
@@ -190,10 +183,11 @@ public class BitbucketTests implements BasicTests, MissingCommitsTests, Activity
         rpc.getPage().deleteAllOrganizations();
 
         // check that postcommit hook is removed
-        servicesConfig = getBitbucketServices(bitbucketServiceConfigUrl, "jirabitbucketconnector", PasswordUtil.getPassword("jirabitbucketconnector"));
+        servicesConfig = HttpSenderUtils.makeHttpRequest(new GetMethod(bitbucketServiceConfigUrl),
+                "jirabitbucketconnector", PasswordUtil.getPassword("jirabitbucketconnector"));
         assertThat(servicesConfig).doesNotContain(syncUrl);
     }
-    
+
     @Test
     @Override
     public void testActivityPresentedForQA5()
@@ -209,7 +203,7 @@ public class BitbucketTests implements BasicTests, MissingCommitsTests, Activity
         WebElement iframeElm = jira.getTester().getDriver().getDriver().findElement(By.id("gadget-10001"));
         String iframeSrc = iframeElm.getAttribute("src");
         jira.getTester().gotoUrl(iframeSrc);
-        
+
         page = jira.getPageBinder().bind(DashboardActivityStreamsPage.class);
 
         // Activity streams should contain at least one changeset with 'more files' link.
@@ -234,7 +228,7 @@ public class BitbucketTests implements BasicTests, MissingCommitsTests, Activity
         page = jira.visit(DashboardActivityStreamsPage.class);
         page.checkIssueActivityNotPresentedForQA5();
     }
-    
+
     @Override
     @Test
     public void testAnonymousAccess()
@@ -266,15 +260,15 @@ public class BitbucketTests implements BasicTests, MissingCommitsTests, Activity
         page = jira.getPageBinder().bind(DashboardActivityStreamsPage.class);
         page.checkIssueActivityNotPresentedForQA5();
 
-        // log in to JIRA 
+        // log in to JIRA
         new JiraLoginPageController(jira).login();
         setupAnonymousAccessForbidden();
     }
-    
+
     //-------------------------------------------------------------------
-    //--------- these methods should go to some common utility/class ---- 
+    //--------- these methods should go to some common utility/class ----
     //-------------------------------------------------------------------
- 
+
     private OAuthCredentials getOAuthCredentials()
     {
         return new OAuthCredentials(oAuth.key, oAuth.secret);
@@ -304,60 +298,13 @@ public class BitbucketTests implements BasicTests, MissingCommitsTests, Activity
         jira.getTester().getDriver().waitUntilElementIsVisible(By.id("delete_submit"));
         jira.getTester().getDriver().findElement(By.id("delete_submit")).click();
     }
-    
-    //-------------------------------------------------------------------
-    //--------- these methods should go probably to bitbucket client ---- 
-    //-------------------------------------------------------------------
-    
-    private void removeExistingServices(String url, String username, String password)
-    {
-        String bitbucketServices = getBitbucketServices(url, username, password);
-        
-        String regexp = "\"id\":.([0-9]*)";
-        Matcher m = Pattern.compile(regexp).matcher(bitbucketServices);
-        while (m.find())
-        {
-            String id = m.group(1);
-            removeService(url+"/"+id, username, password);
-        }
-    }
-    
-    private String removeService(String url, String username, String password)
-    {
-        HttpMethod method = new DeleteMethod(url);
-        return makeHttpRequest(username, password, method);
-    }
-
-    private String getBitbucketServices(String url, String username, String password)
-    {
-        HttpMethod method = new GetMethod(url);
-        return makeHttpRequest(username, password, method);
-    }
-
-    private String makeHttpRequest(String username, String password, HttpMethod method)
-    {
-        try
-        {
-            HttpClient httpClient = new HttpClient();
-            
-            AuthScope authScope = new AuthScope(method.getURI().getHost(), AuthScope.ANY_PORT, null, AuthScope.ANY_SCHEME);
-            httpClient.getParams().setAuthenticationPreemptive(true);
-            httpClient.getState().setCredentials(authScope, new UsernamePasswordCredentials(username, password));
-            
-            httpClient.executeMethod(method);
-            return method.getResponseBodyAsString();
-        } catch (IOException e)
-        {
-            throw new RuntimeException(e);
-        }
-    }
 
     @Override
     public void shouldBeAbleToSeePrivateRepositoriesFromTeamAccount()
     {
-        // Not relevant for Bitbucket - is uses same api for organizations ans users
-        // but maybe we add something heee one day
+        // Not relevant for Bitbucket - it uses same API for organizations and users
+        // but maybe we will add something here one day
     }
 
-   
+
 }
