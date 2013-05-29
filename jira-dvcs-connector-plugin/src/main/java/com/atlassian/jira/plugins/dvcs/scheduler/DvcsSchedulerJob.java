af3fd772e3c555d9606c9a5eb968c6eb1be940b6
Stanislav Dvorscak <sdvorscak@atlassian.com>
2013-05-29 12:06:05 +0200

    Changeset was moved to issue/BBC-531_v1.4.1

--- jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/scheduler/DvcsSchedulerJob.java
+++ jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/scheduler/DvcsSchedulerJob.java
@@ -1,6 +1,5 @@
 package com.atlassian.jira.plugins.dvcs.scheduler;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -8,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.atlassian.jira.plugins.dvcs.model.Organization;
-import com.atlassian.jira.plugins.dvcs.model.Repository;
 import com.atlassian.jira.plugins.dvcs.service.OrganizationService;
 import com.atlassian.jira.plugins.dvcs.service.RepositoryService;
 import com.atlassian.sal.api.scheduling.PluginJob;
@@ -17,49 +15,18 @@ public class DvcsSchedulerJob implements PluginJob
 {
     private static final Logger log = LoggerFactory.getLogger(DvcsSchedulerJob.class);
 
-    @Override
+	@Override
     public void execute(Map<String, Object> jobDataMap)
-    {
-        log.debug("Running DvcsSchedulerJob ");
-
-        OrganizationService organizationService = (OrganizationService) jobDataMap.get("organizationService");
-        RepositoryService repositoryService = (RepositoryService) jobDataMap.get("repositoryService");
-        syncOrganizations(organizationService, repositoryService);
-        cleanOrphanRepositories(organizationService, repositoryService);
-    }
-
-    /**
-     * Synchronizes all organizations.
-     * 
-     * @param organizationService
-     * @param repositoryService
-     */
-    private void syncOrganizations(OrganizationService organizationService, RepositoryService repositoryService)
-    {
-        List<Organization> organizations = organizationService.getAll(false);
-        for (Organization organization : organizations)
+    {   
+		log.debug("Running DvcsSchedulerJob ");
+		
+		OrganizationService organizationService = (OrganizationService) jobDataMap.get("organizationService");
+		RepositoryService repositoryService = (RepositoryService) jobDataMap.get("repositoryService");
+		List<Organization> organizations = organizationService.getAll(false);
+		for (Organization organization : organizations)
         {
-            repositoryService.syncRepositoryList(organization);
+			repositoryService.syncRepositoryList(organization);
         }
     }
-
-    /**
-     * Cleans orphan repositories - repositories mark as deleted with not existing organization.
-     * 
-     * @param organizationService
-     * @param repositoryService
-     */
-    private void cleanOrphanRepositories(OrganizationService organizationService, RepositoryService repositoryService)
-    {
-        List<Repository> orphanRepositories = new LinkedList<Repository>();
-        for (Repository repository : repositoryService.getAllRepositories(true))
-        {
-            if (repository.isDeleted() && organizationService.get(repository.getOrganizationId(), false) == null)
-            {
-                orphanRepositories.add(repository);
-            }
-        }
-        repositoryService.removeOrphanRepositories(orphanRepositories);
-    }
-
+	
 }
