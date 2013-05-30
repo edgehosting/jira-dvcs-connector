3c39bc8eea281daf0933781b816519ec94ac2649
Miroslav Stencel <mstencel@atlassian.com>
2013-05-30 17:08:56 +0200

    BBC-539 Adding tooltip to forks drop down, adding radio showing which repository is chosen

--- jira-dvcs-connector-plugin/src/main/resources/javascripts/dvcs-issue-tabs.js
+++ jira-dvcs-connector-plugin/src/main/resources/javascripts/dvcs-issue-tabs.js
@@ -1,4 +1,4 @@
-function showChangesetByFork(node, repo) {
+function showChangesetByFork(node, repoId, repoName, repoUrl) {
 
 
     // hide all changesets CommitPanels
@@ -7,6 +7,22 @@ function showChangesetByFork(node, repo) {
     });
 
     // show only that which is from given repository
-    AJS.$('#' + node + "-" + repo).show();
+    AJS.$('#' + node + "-" + repoId).show();
+    
+    registerForksTooltip(node, repoName, repoUrl);
 }
 
+function registerForksTooltip(node, repoName, repoUrl) {
+	registerInlineDialogTooltip(AJS.$("#fork-drop-down-"+node), "This changeset is present in multiple repositories (forks). Currently showing <a href='"+repoUrl+"' target='_blank'>" + repoName + "</a>.");
+}
+
+function registerInlineDialogTooltip(element, body) {
+    return AJS.InlineDialog(AJS.$(element), "tooltip_"+AJS.$(element).attr('id'),
+            function(content, trigger, showPopup) {
+                content.css({"padding":"10px"}).html(body);
+                showPopup();
+                return false;
+            },
+            {onHover:true, hideDelay:200, showDelay:1000, arrowOffsetX:-8, offsetX:-80}
+        );
+}
