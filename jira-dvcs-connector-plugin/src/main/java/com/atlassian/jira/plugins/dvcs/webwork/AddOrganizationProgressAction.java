cfe244b02ef5e69f8bd6e0e58682ab049fd03931
Miroslav Stencel <mstencel@atlassian.com>
2013-06-28 15:16:51 +0200

    BBC-559 Formatting

--- jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/webwork/AddOrganizationProgressAction.java
+++ jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/webwork/AddOrganizationProgressAction.java
@@ -9,46 +9,45 @@ import com.atlassian.sal.api.ApplicationProperties;
 
 public class AddOrganizationProgressAction extends JiraWebActionSupport
 {
-	private static final long serialVersionUID = -8035393686536929940L;
-	private final ApplicationProperties ap;
-	private static Map<String, String> typeToRedirectUrls = new HashMap<String, String>();
-	static
-	{
-		typeToRedirectUrls.put("1", "AddBitbucketOrganization");
-		typeToRedirectUrls.put("2", "AddGithubOrganization");
-		typeToRedirectUrls.put("3", "AddGithubEnterpriseOrganization");
-	}
+    private static final long serialVersionUID = -8035393686536929940L;
+    private final ApplicationProperties ap;
+    private static Map<String, String> typeToRedirectUrls = new HashMap<String, String>();
+    static
+    {
+        typeToRedirectUrls.put("1", "AddBitbucketOrganization");
+        typeToRedirectUrls.put("2", "AddGithubOrganization");
+        typeToRedirectUrls.put("3", "AddGithubEnterpriseOrganization");
+    }
 
-	public AddOrganizationProgressAction(ApplicationProperties ap)
-	{
-		super();
-		this.ap = ap;
-	}
+    public AddOrganizationProgressAction(ApplicationProperties ap)
+    {
+        super();
+        this.ap = ap;
+    }
 
-	@Override
-	public String doDefault() throws Exception
-	{
-		return super.doDefault();
-	}
+    @Override
+    public String doDefault() throws Exception
+    {
+        return super.doDefault();
+    }
 
-	public String doFinish() throws Exception
-	{
-		return doDefault();
-	}
+    public String doFinish() throws Exception
+    {
+        return doDefault();
+    }
 
-	public String getCurrentUrl()
-	{
-	    // using request directly because of compatibility with Jira 5.2
-		String redirectEndpoint = typeToRedirectUrls.get(this.request.getParameter("t"));
-		if (redirectEndpoint != null)
-		{
-			return ap.getBaseUrl()
-			        + "/secure/admin/" + redirectEndpoint + "!finish.jspa?"
-			        + this.request.getQueryString();
-		} else
-		{
-			return SystemUtils.getRedirect(this, "ConfigureDvcsOrganizations.jspa?atl_token=" + getXsrfToken(), false);
-		}
-	}
-
+    public String getCurrentUrl()
+    {
+        // using request directly because of compatibility with Jira 5.2
+        String redirectEndpoint = typeToRedirectUrls.get(this.request.getParameter("t"));
+        if (redirectEndpoint != null)
+        {
+            return ap.getBaseUrl()
+                    + "/secure/admin/" + redirectEndpoint + "!finish.jspa?"
+                    + this.request.getQueryString();
+        } else
+        {
+            return SystemUtils.getRedirect(this, "ConfigureDvcsOrganizations.jspa?atl_token=" + getXsrfToken(), false);
+        }
+    }
 }
