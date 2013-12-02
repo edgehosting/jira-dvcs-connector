91e538e3daa16f1bcf6826570a1659383bc8bfdb
Stanislav Dvorscak <sdvorscak@atlassian.com>
2013-12-02 16:26:50 +0100

    BBC-644: PSQLException: ERROR: column "id" does not exist

--- jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/activeobjects/QueryHelperImpl.java
+++ jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/activeobjects/QueryHelperImpl.java
@@ -2,10 +2,17 @@ package com.atlassian.jira.plugins.dvcs.activeobjects;
 
 import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
+import java.util.Arrays;
+
+import javax.annotation.Resource;
 
 import org.apache.commons.lang.StringUtils;
 
 import com.atlassian.activeobjects.spi.DataSourceProvider;
+import com.atlassian.plugin.PluginAccessor;
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Iterables;
 
 /**
  * An implementation of {@link QueryHelper}.
@@ -17,8 +24,15 @@ public class QueryHelperImpl implements QueryHelper
 {
 
     /**
-     * @see #QueryHelperImpl(DataSourceProvider)
+     * Injected {@link PluginAccessor} dependency.
      */
+    @Resource
+    private PluginAccessor pluginAccessor;
+
+    /**
+     * Injected {@link DataSourceProvider} dependency.
+     */
+    @Resource
     private DataSourceProvider dataSourceProvider;
 
     /**
@@ -35,17 +49,6 @@ public class QueryHelperImpl implements QueryHelper
      * @see #init()
      */
     private boolean initialized;
-
-    /**
-     * Constructor.
-     * 
-     * @param dataSourceProvider
-     *            injected {@link DataSourceProvider} dependency
-     */
-    public QueryHelperImpl(DataSourceProvider dataSourceProvider)
-    {
-        this.dataSourceProvider = dataSourceProvider;
-    }
 
     /**
      * Lazy initialization of information provided by connection.
@@ -113,6 +116,101 @@ public class QueryHelperImpl implements QueryHelper
             return plainColumnName;
         }
 
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getOrder(OrderClause... orderClause)
+    {
+
+        Function<OrderClause, String> orderClauseToString;
+
+        if (isBugAOSupportOnlySingleColumnInsideOrderClause())
+        {
+            orderClauseToString = new Function<OrderClause, String>()
+            {
+
+                private boolean isFirst = true;
+
+                @Override
+                public String apply(OrderClause input)
+                {
+                    if (isFirst)
+                    {
+                        isFirst = false;
+                        return input.getColumn() + ' ' + input.getOrder().name();
+                    } else
+                    {
+                        return getSqlColumnName(input.getColumn()) + ' ' + input.getOrder().name();
+                    }
+                }
+
+            };
+        } else
+        {
+            orderClauseToString = new Function<OrderClause, String>()
+            {
+
+                @Override
+                public String apply(OrderClause input)
+                {
+                    return input.getColumn() + ' ' + input.getOrder().name();
+                }
+
+            };
+        }
+
+        return Joiner.on(", ").join(Iterables.transform(Arrays.asList(orderClause), orderClauseToString));
+    }
+
+    /**
+     * @return True if AO support only single column inside order clause.
+     */
+    private boolean isBugAOSupportOnlySingleColumnInsideOrderClause()
+    {
+        return isBeforeAOVersion("0.20");
+    }
+
+    /**
+     * Constrain for AO version - current AO version must be less than provided version.
+     * 
+     * @param version
+     *            expected version constrain
+     * @return true if current version is less than provided
+     */
+    private boolean isBeforeAOVersion(String version)
+    {
+        String aoVersion = pluginAccessor.getPlugin("com.atlassian.activeobjects.activeobjects-plugin").getPluginInformation().getVersion();
+
+        String[] aoVersionComponents = aoVersion.split("\\.");
+        String[] versionCompoennts = version.split("\\.");
+
+        for (int i = 0; i < versionCompoennts.length; i++)
+        {
+            if (aoVersionComponents.length < i)
+            {
+                break;
+            }
+
+            try
+            {
+                boolean isLast = versionCompoennts.length == i;
+                int aoVersionComponent = Integer.parseInt(aoVersionComponents[i]);
+                int versionComponent = Integer.parseInt(versionCompoennts[i]);
+
+                if (aoVersionComponent > versionComponent || (isLast && aoVersionComponent == versionComponent))
+                {
+                    return false;
+                }
+            } catch (NumberFormatException e)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
