7ee13c47307ae415941c46b47651902bb59abce0
Miroslav Stencel <mstencel@atlassian.com>
2013-12-03 19:15:18 +0100

    FUSE-586 Fixing tests

--- jira-dvcs-connector-plugin/src/test/java/com/atlassian/jira/plugins/dvcs/spi/bitbucket/BitbucketCommunicatorTest.java
+++ jira-dvcs-connector-plugin/src/test/java/com/atlassian/jira/plugins/dvcs/spi/bitbucket/BitbucketCommunicatorTest.java
@@ -189,6 +189,12 @@ public class BitbucketCommunicatorTest
             throw new UnsupportedOperationException("Not implemented");
         }
 
+        @Override
+        public List<Branch> getBranchesForIssue(final Iterable<String> issueKeys, final String dvcsType)
+        {
+            throw new UnsupportedOperationException("Not implemented");
+        }
+
         public List<String> getHeads(int repositoryId)
         {
             return Lists.transform(heads.get(repositoryId), new Function<BranchHead, String>()
