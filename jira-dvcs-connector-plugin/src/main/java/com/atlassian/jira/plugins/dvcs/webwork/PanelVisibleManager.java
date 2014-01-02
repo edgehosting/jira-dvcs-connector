130efa244c81c2ee8a67e567c3ead4afb2f1ff87
Charles O'Farrell <cofarrell@atlassian.com>
2014-01-03 09:26:07 +1100

    FUSE-700 Remove condition for Github being connected

--- jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/webwork/PanelVisibleManager.java
+++ jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/webwork/PanelVisibleManager.java
@@ -3,9 +3,6 @@ package com.atlassian.jira.plugins.dvcs.webwork;
 import com.atlassian.crowd.embedded.api.User;
 import com.atlassian.jira.config.FeatureManager;
 import com.atlassian.jira.issue.Issue;
-import com.atlassian.jira.plugins.dvcs.service.OrganizationService;
-import com.atlassian.jira.plugins.dvcs.spi.github.GithubCommunicator;
-import com.atlassian.jira.plugins.dvcs.spi.githubenterprise.GithubEnterpriseCommunicator;
 import com.atlassian.jira.security.PermissionManager;
 import com.atlassian.jira.security.Permissions;
 import com.atlassian.jira.user.ApplicationUsers;
@@ -17,14 +14,12 @@ public class PanelVisibleManager
     private static final String LABS_OPT_IN = "jira.plugin.devstatus.phasetwo";
 
     private final FeatureManager featureManager;
-    private final OrganizationService organizationService;
     private final PermissionManager permissionManager;
     private final PluginAccessor pluginAccessor;
 
-    public PanelVisibleManager(FeatureManager featureManager, OrganizationService organizationService, PermissionManager permissionManager, PluginAccessor pluginAccessor)
+    public PanelVisibleManager(FeatureManager featureManager, PermissionManager permissionManager, PluginAccessor pluginAccessor)
     {
         this.featureManager = featureManager;
-        this.organizationService = organizationService;
         this.permissionManager = permissionManager;
         this.pluginAccessor = pluginAccessor;
     }
@@ -35,11 +30,6 @@ public class PanelVisibleManager
                 // JIRA 6.1.x was installed with 0.x of the devsummary plugin, everything else after will want to hide this panel
                 pluginAccessor.getPlugin(DEVSUMMARY_PLUGIN_ID).getPluginInformation().getVersion().startsWith("0.")) &&
                 permissionManager.hasPermission(Permissions.VIEW_VERSION_CONTROL, issue, user) &&
-                (!featureManager.isEnabledForUser(ApplicationUsers.from(user), LABS_OPT_IN) || isGithubConnected());
-    }
-
-    private boolean isGithubConnected()
-    {
-        return organizationService.existsOrganizationWithType(GithubCommunicator.GITHUB, GithubEnterpriseCommunicator.GITHUB_ENTERPRISE);
+                !featureManager.isEnabledForUser(ApplicationUsers.from(user), LABS_OPT_IN);
     }
 }
