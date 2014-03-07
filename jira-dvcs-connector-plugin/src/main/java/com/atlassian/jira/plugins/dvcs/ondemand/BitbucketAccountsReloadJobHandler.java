3c55251c3242c26a0b5f66afc6fb67bd2615a116
Andrew Swan <aswan@atlassian.com>
2014-03-07 17:43:25 +1100

    JDEV-26465 Fixing intermittent circular dependencies by looking up the bean after the app context is initialized

--- jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/ondemand/BitbucketAccountsReloadJobHandler.java
+++ jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/ondemand/BitbucketAccountsReloadJobHandler.java
@@ -2,22 +2,36 @@ package com.atlassian.jira.plugins.dvcs.ondemand;
 
 import com.atlassian.scheduler.compat.JobHandler;
 import com.atlassian.scheduler.compat.JobInfo;
+import org.apache.commons.lang3.Validate;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
+import java.util.Collection;
 
 /**
  * Runs the scheduled job of reloading Bitbucket accounts.
  */
 @Component
-public class BitbucketAccountsReloadJobHandler implements JobHandler
+public class BitbucketAccountsReloadJobHandler implements ApplicationContextAware, JobHandler
 {
-    @Resource
-    private BitbucketAccountsConfigService accountsConfigService;
+    private BitbucketAccountsConfigService configService;
+
+    @Override
+    public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException
+    {
+        // Not injecting this via DI because it gives intermittent circular dependencies
+        @SuppressWarnings("unchecked")
+        final Collection<BitbucketAccountsConfigService> services =
+                applicationContext.getBeansOfType(BitbucketAccountsConfigService.class).values();
+        Validate.isTrue(services.size() == 1, "Expected one service but found %d: %s", services.size(), services);
+        this.configService = services.iterator().next();
+    }
 
     @Override
     public void execute(final JobInfo jobInfo)
     {
-        accountsConfigService.reload();
+        configService.reload();
     }
 }
