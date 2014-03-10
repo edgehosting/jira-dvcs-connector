14642249bf8b86c178e8f4c8c7ce3217947813d0
Andrew Swan <aswan@atlassian.com>
2014-03-10 12:29:59 +1100

    JDEV-26465 Delaying component lookup until execution time, to solve lookup failure

--- jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/ondemand/BitbucketAccountsReloadJobHandler.java
+++ jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/ondemand/BitbucketAccountsReloadJobHandler.java
@@ -16,22 +16,27 @@ import java.util.Collection;
 @Component
 public class BitbucketAccountsReloadJobHandler implements ApplicationContextAware, JobHandler
 {
-    private BitbucketAccountsConfigService configService;
+    private ApplicationContext applicationContext;
 
     @Override
     public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException
+    {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void execute(final JobInfo jobInfo)
+    {
+        getConfigService().reload();
+    }
+
+    private BitbucketAccountsConfigService getConfigService()
     {
         // Not injecting this via DI because it gives intermittent circular dependencies
         @SuppressWarnings("unchecked")
         final Collection<BitbucketAccountsConfigService> services =
                 applicationContext.getBeansOfType(BitbucketAccountsConfigService.class).values();
         Validate.isTrue(services.size() == 1, "Expected one service but found %d: %s", services.size(), services);
-        this.configService = services.iterator().next();
-    }
-
-    @Override
-    public void execute(final JobInfo jobInfo)
-    {
-        configService.reload();
+        return services.iterator().next();
     }
 }
