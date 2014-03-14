9415aa0a40463ea7bf7221a3d444cf17322878c2
Miroslav Stencel <mstencel@atlassian.com>
2014-03-14 13:15:55 +0100

    FUSE-708 updating test after optimisation

--- jira-dvcs-connector-plugin/src/test/java/com/atlassian/jira/plugins/dvcs/dao/impl/transform/ChangesetTransformerTest.java
+++ jira-dvcs-connector-plugin/src/test/java/com/atlassian/jira/plugins/dvcs/dao/impl/transform/ChangesetTransformerTest.java
@@ -1,35 +1,30 @@
 package com.atlassian.jira.plugins.dvcs.dao.impl.transform;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Matchers.isA;
-import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
-
-import java.util.Collection;
-import java.util.List;
-
+import com.atlassian.activeobjects.external.ActiveObjects;
+import com.atlassian.jira.plugins.dvcs.activeobjects.v3.ChangesetMapping;
+import com.atlassian.jira.plugins.dvcs.activeobjects.v3.RepositoryToChangesetMapping;
+import com.atlassian.sal.api.transaction.TransactionCallback;
+import com.google.common.collect.Lists;
+import com.google.common.collect.ObjectArrays;
 import net.java.ao.Query;
-
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.atlassian.activeobjects.external.ActiveObjects;
-import com.atlassian.jira.plugins.dvcs.activeobjects.v3.ChangesetMapping;
-import com.atlassian.jira.plugins.dvcs.activeobjects.v3.OrganizationMapping;
-import com.atlassian.jira.plugins.dvcs.activeobjects.v3.RepositoryMapping;
-import com.atlassian.jira.plugins.dvcs.activeobjects.v3.RepositoryToChangesetMapping;
-import com.google.common.collect.Lists;
+import java.util.Collection;
+import java.util.List;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 public class ChangesetTransformerTest
 {
@@ -38,9 +33,6 @@ public class ChangesetTransformerTest
 
     @InjectMocks
     private ChangesetTransformer transformer;
-
-    @Captor
-    private ArgumentCaptor<Query> query;
 
     @BeforeMethod(alwaysRun = true)
     public void setup()
@@ -51,66 +43,47 @@ public class ChangesetTransformerTest
     @Test
     public void testTransformAllShouldPreloadRepositories()
     {
-        final RepositoryMapping[] sampleRepos = sampleRepos();
-        final OrganizationMapping sampleOrg = sampleOrg();
-
-        when(aoMock.find(eq(RepositoryMapping.class), isA(Query.class))).thenReturn(sampleRepos);
-        when(aoMock.get(OrganizationMapping.class, 8)).thenReturn(sampleOrg);
+        when(aoMock.executeInTransaction(isA(TransactionCallback.class))).thenAnswer(
+                new Answer<Object>()
+                {
+                    @SuppressWarnings("rawtypes")
+                    @Override
+                    public Object answer(InvocationOnMock invocationOnMock) throws Throwable
+                    {
+                        return ((TransactionCallback) invocationOnMock.getArguments()[0]).doInTransaction();
+                    }
+                });
 
         transformer.transformAll(changesetMappings(), 0, "bitbucket");
 
-        verify(aoMock, atLeastOnce()).get(OrganizationMapping.class, 8);
-        verify(aoMock, times(1)).find(eq(RepositoryMapping.class), query.capture());
+        verify(aoMock).executeInTransaction(any(TransactionCallback.class));
+        verify(aoMock).find(eq(RepositoryToChangesetMapping.class), any(Query.class));
         verifyNoMoreInteractions(aoMock);
-
-        assertThat(query.getValue().getWhereClause(), is("ID IN (1, 2, 3, 4, 5, 6)"));
-    }
-
-    private OrganizationMapping sampleOrg()
-    {
-        final OrganizationMapping orgSampleMock = Mockito.mock(OrganizationMapping.class);
-        when(orgSampleMock.getDvcsType()).thenReturn("bitbucket");
-        return orgSampleMock;
-    }
-
-    private RepositoryMapping[] sampleRepos()
-    {
-        final List<RepositoryMapping> repos = Lists.newArrayList();
-        for (int i = 1; i <= 6; i++)
-        {
-            repos.add(repoMock(Mockito.mock(RepositoryMapping.class), i));
-        }
-        return repos.toArray(new RepositoryMapping[] {});
-    }
-
-    private RepositoryMapping repoMock(final RepositoryMapping mock, final int id)
-    {
-        when(mock.isDeleted()).thenReturn(false);
-        when(mock.isLinked()).thenReturn(true);
-        when(mock.getID()).thenReturn(id);
-        when(mock.getOrganizationId()).thenReturn(8);
-        return mock;
     }
 
     private Collection<ChangesetMapping> changesetMappings()
     {
         final ChangesetMapping csetSampleMock = Mockito.mock(ChangesetMapping.class);
-        final RepositoryToChangesetMapping[] linkMappingsSampleMock = linkMappings(1, 2, 3);
+        final RepositoryToChangesetMapping[] linkMappingsSampleMock = linkMappings(csetSampleMock, 1, 2, 3);
         when(csetSampleMock.getRepositoryIdMappings()).thenReturn(linkMappingsSampleMock);
 
         final ChangesetMapping cset2SampleMock = Mockito.mock(ChangesetMapping.class);
-        final RepositoryToChangesetMapping[] link2MappingsSampleMock = linkMappings(3, 4, 5, 6);
+        final RepositoryToChangesetMapping[] link2MappingsSampleMock = linkMappings(cset2SampleMock, 3, 4, 5, 6);
         when(cset2SampleMock.getRepositoryIdMappings()).thenReturn(link2MappingsSampleMock);
+
+        when(aoMock.find(eq(RepositoryToChangesetMapping.class), isA(Query.class))).thenReturn(ObjectArrays.concat(linkMappingsSampleMock, link2MappingsSampleMock, RepositoryToChangesetMapping.class));
+
         return Lists.newArrayList(csetSampleMock, cset2SampleMock);
     }
 
-    private RepositoryToChangesetMapping[] linkMappings(final Integer... repoIds)
+    private RepositoryToChangesetMapping[] linkMappings(final ChangesetMapping changesetMapping, final Integer... repoIds)
     {
         final List<RepositoryToChangesetMapping> repos = Lists.newArrayList();
         for (final Integer repoId : repoIds)
         {
             final RepositoryToChangesetMapping linkSampleMock = Mockito.mock(RepositoryToChangesetMapping.class);
             when(linkSampleMock.getRepositoryId()).thenReturn(repoId);
+            when(linkSampleMock.getChangeset()).thenReturn(changesetMapping);
             repos.add(linkSampleMock);
 
         }
