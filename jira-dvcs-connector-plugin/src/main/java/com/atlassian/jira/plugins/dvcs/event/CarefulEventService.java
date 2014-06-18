742089ce678554ccf8e4fe8cf6f50639eb4db949
Luis Miranda <lmiranda@atlassian.com>
2014-06-18 18:22:30 +1000

    BBC-844: fixed innacurate Javadoc

--- jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/event/CarefulEventService.java
+++ jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/event/CarefulEventService.java
@@ -2,8 +2,6 @@ package com.atlassian.jira.plugins.dvcs.event;
 
 /**
  * An EventService that respects the <code>{@value com.atlassian.jira.plugins.dvcs.event.EventsFeature#FEATURE_NAME}</code> kill switch.
- *
- * @since v6.1
  */
 public interface CarefulEventService extends EventService
 {
