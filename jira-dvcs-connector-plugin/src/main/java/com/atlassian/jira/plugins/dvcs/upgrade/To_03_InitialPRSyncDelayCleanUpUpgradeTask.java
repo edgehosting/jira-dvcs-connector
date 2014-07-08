f9c87a72ac07f4046de7be30d08631f363b8dc79
Miroslav Stencel <mstencel@atlassian.com>
2014-07-08 14:05:28 +0200

    BBC-753 Upgrade task for cleaning up initial delay property

---
+++ jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/upgrade/To_03_InitialPRSyncDelayCleanUpUpgradeTask.java
@@ -0,0 +1,55 @@
+package com.atlassian.jira.plugins.dvcs.upgrade;
+
+import com.atlassian.sal.api.message.Message;
+import com.atlassian.sal.api.pluginsettings.PluginSettings;
+import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
+import com.atlassian.sal.api.upgrade.PluginUpgradeTask;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * Cleaning up plugin property for initial pull request delay
+ *
+ */
+public class To_03_InitialPRSyncDelayCleanUpUpgradeTask implements PluginUpgradeTask
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(To_03_InitialPRSyncDelayCleanUpUpgradeTask.class);
+    private static final String POSTPONE_PR_SYNC_UNTIL = "plugin.dvcs.prsyncpostpone";
+
+    private final PluginSettings pluginSettings;
+
+    public To_03_InitialPRSyncDelayCleanUpUpgradeTask(final PluginSettingsFactory pluginSettingsFactory)
+    {
+        this.pluginSettings = pluginSettingsFactory.createGlobalSettings();
+    }
+
+    @Override
+    public Collection<Message> doUpgrade() throws Exception
+    {
+        LOGGER.debug("Removing '{}' property", POSTPONE_PR_SYNC_UNTIL);
+        pluginSettings.remove(POSTPONE_PR_SYNC_UNTIL);
+
+        return Collections.emptyList();
+    }
+
+    @Override
+    public int getBuildNumber()
+    {
+        return 3;
+    }
+
+    @Override
+    public String getShortDescription()
+    {
+        return "Cleans up 'plugin.dvcs.prsyncpostpone' plugin property";
+    }
+
+    @Override
+    public String getPluginKey()
+    {
+        return "com.atlassian.jira.plugins.jira-bitbucket-connector-plugin";
+    }
+}
