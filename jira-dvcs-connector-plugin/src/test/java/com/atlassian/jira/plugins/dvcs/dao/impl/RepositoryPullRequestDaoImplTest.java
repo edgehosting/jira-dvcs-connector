43f14647c6b95b31ca5e6a9a15ad8b6fad76ab62
Zehua Liu <zliu@atlassian.com>
2014-08-18 10:01:56 +1000

    BBC-877 refactored tests for pull request dao

--- jira-dvcs-connector-plugin/src/test/java/com/atlassian/jira/plugins/dvcs/dao/impl/RepositoryPullRequestDaoImplTest.java
+++ jira-dvcs-connector-plugin/src/test/java/com/atlassian/jira/plugins/dvcs/dao/impl/RepositoryPullRequestDaoImplTest.java
@@ -23,6 +23,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -34,21 +35,25 @@ import java.util.Map;
 import java.util.Set;
 import javax.annotation.Nullable;
 
+import static com.atlassian.jira.plugins.dvcs.activity.RepositoryDomainMapping.DOMAIN;
+import static com.atlassian.jira.plugins.dvcs.activity.RepositoryPullRequestIssueKeyMapping.ISSUE_KEY;
+import static com.atlassian.jira.plugins.dvcs.activity.RepositoryPullRequestIssueKeyMapping.PULL_REQUEST_ID;
 import static com.atlassian.jira.plugins.dvcs.matchers.QueryMatchers.isSelect;
+import static com.atlassian.jira.plugins.dvcs.matchers.QueryMatchers.withWhereOnColumns;
 import static com.atlassian.jira.plugins.dvcs.matchers.QueryMatchers.withWhereParamsThat;
-import static com.atlassian.jira.plugins.dvcs.matchers.QueryMatchers.withWhereThat;
 import static com.google.common.collect.Iterables.toArray;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyMapOf;
 import static org.mockito.Matchers.argThat;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -57,6 +62,7 @@ import static org.mockito.Mockito.when;
 public class RepositoryPullRequestDaoImplTest
 {
     private static final int REPO_ID = 1;
+    private static final int PR_ID = 1;
 
     @Mock
     ActiveObjects activeObjects;
@@ -70,28 +76,29 @@ public class RepositoryPullRequestDaoImplTest
     @InjectMocks
     RepositoryPullRequestDaoImpl repositoryPullRequestDao;
 
+    @BeforeMethod
+    public void setUp() throws Exception
+    {
+        when(repository.getId()).thenReturn(REPO_ID);
+    }
+
     @Test
     public void testGetIssueKeysWithExistingPullRequestIssueMappings()
     {
         RepositoryPullRequestIssueKeyMapping[] mappingsInDb = toArray(ImmutableList.of(
-                newIssueMapping(1, "ISSUE-1"),
-                newIssueMapping(1, "ISSUE-2"),
-                newIssueMapping(1, "ISSUE-3")),
+                newIssueMapping(PR_ID, "ISSUE-1"),
+                newIssueMapping(PR_ID, "ISSUE-2"),
+                newIssueMapping(PR_ID, "ISSUE-3")),
                 RepositoryPullRequestIssueKeyMapping.class
         );
         when(activeObjects.find(any(Class.class), any(Query.class))).thenReturn(mappingsInDb);
 
-        Set<String> result = repositoryPullRequestDao.getIssueKeys(1, 1);
+        Set<String> result = repositoryPullRequestDao.getIssueKeys(REPO_ID, PR_ID);
 
         assertThat("Result should be never null", result, notNullValue());
         assertThat("Result should contain 3 items", result, hasSize(3));
         assertThat(result, containsInAnyOrder("ISSUE-1", "ISSUE-2", "ISSUE-3"));
-        verify(activeObjects).find(eq(RepositoryPullRequestIssueKeyMapping.class), argThat(Matchers.<Query>allOf(
-                isSelect(),
-                withWhereThat(containsString(RepositoryPullRequestIssueKeyMapping.DOMAIN)),
-                withWhereThat(containsString(RepositoryPullRequestIssueKeyMapping.PULL_REQUEST_ID)),
-                withWhereParamsThat(Matchers.<Object>contains(1, 1))
-        )));
+        verify(activeObjects).find(eq(RepositoryPullRequestIssueKeyMapping.class), matchQueryForPRIssueKey());
     }
 
     @Test
@@ -100,16 +107,11 @@ public class RepositoryPullRequestDaoImplTest
         when(activeObjects.find(any(Class.class), any(Query.class)))
                 .thenReturn(new RepositoryPullRequestIssueKeyMapping[0]);
 
-        Set<String> result = repositoryPullRequestDao.getIssueKeys(1, 1);
+        Set<String> result = repositoryPullRequestDao.getIssueKeys(REPO_ID, PR_ID);
 
         assertThat("Result should be never null", result, notNullValue());
         assertThat("Result should be empty", result, hasSize(0));
-        verify(activeObjects).find(eq(RepositoryPullRequestIssueKeyMapping.class), argThat(Matchers.<Query>allOf(
-                isSelect(),
-                withWhereThat(containsString(RepositoryPullRequestIssueKeyMapping.DOMAIN)),
-                withWhereThat(containsString(RepositoryPullRequestIssueKeyMapping.PULL_REQUEST_ID)),
-                withWhereParamsThat(Matchers.<Object>contains(1, 1))
-        )));
+        verify(activeObjects).find(eq(RepositoryPullRequestIssueKeyMapping.class), matchQueryForPRIssueKey());
     }
 
     @Test
@@ -122,7 +124,7 @@ public class RepositoryPullRequestDaoImplTest
             @Override
             public Object answer(final InvocationOnMock invocation) throws Throwable
             {
-                ((TransactionCallback)(invocation.getArguments()[0])).doInTransaction();
+                ((TransactionCallback) (invocation.getArguments()[0])).doInTransaction();
                 return null;
             }
         });
@@ -139,9 +141,9 @@ public class RepositoryPullRequestDaoImplTest
         mockExistingPRIssueKeyMappings("TEST-2", "TEST-3");
 
         assertThat(repositoryPullRequestDao.updatePullRequestIssueKeys(repository, REPO_ID), equalTo(4));
-        // TODO use the matchers on master branch for more precise matching
-        verify(activeObjects, times(2)).create(eq(RepositoryPullRequestIssueKeyMapping.class), anyMapOf(String.class, Object.class));
-        verify(activeObjects, times(0)).delete(org.mockito.Matchers.<RawEntity<?>[]>anyVararg());
+        verify(activeObjects).create(eq(RepositoryPullRequestIssueKeyMapping.class), matchMapHasIssueKey("TITLE-1"));
+        verify(activeObjects).create(eq(RepositoryPullRequestIssueKeyMapping.class), matchMapHasIssueKey("SRCBR-1"));
+        verify(activeObjects, never()).delete(org.mockito.Matchers.<RawEntity<?>[]>anyVararg());
     }
 
     @Test
@@ -151,8 +153,10 @@ public class RepositoryPullRequestDaoImplTest
         mockExistingPRIssueKeyMappings("TEST-2", "TEST-3");
 
         assertThat(repositoryPullRequestDao.updatePullRequestIssueKeys(repository, REPO_ID), equalTo(5));
-        verify(activeObjects, times(3)).create(eq(RepositoryPullRequestIssueKeyMapping.class), anyMapOf(String.class, Object.class));
-        verify(activeObjects, times(0)).delete(org.mockito.Matchers.<RawEntity<?>[]>anyVararg());
+        verify(activeObjects).create(eq(RepositoryPullRequestIssueKeyMapping.class), matchMapHasIssueKey("TITLE-1"));
+        verify(activeObjects).create(eq(RepositoryPullRequestIssueKeyMapping.class), matchMapHasIssueKey("SRCBR-1"));
+        verify(activeObjects).create(eq(RepositoryPullRequestIssueKeyMapping.class), matchMapHasIssueKey("TEST-1"));
+        verify(activeObjects, never()).delete(org.mockito.Matchers.<RawEntity<?>[]>anyVararg());
     }
 
     @Test
@@ -162,8 +166,37 @@ public class RepositoryPullRequestDaoImplTest
         mockExistingPRIssueKeyMappings("TEST-2", "TEST-3", "TEST-4", "TEST-5");
 
         assertThat(repositoryPullRequestDao.updatePullRequestIssueKeys(repository, REPO_ID), equalTo(5));
-        verify(activeObjects, times(3)).create(eq(RepositoryPullRequestIssueKeyMapping.class), anyMapOf(String.class, Object.class));
+        verify(activeObjects).create(eq(RepositoryPullRequestIssueKeyMapping.class), matchMapHasIssueKey("TITLE-1"));
+        verify(activeObjects).create(eq(RepositoryPullRequestIssueKeyMapping.class), matchMapHasIssueKey("SRCBR-1"));
+        verify(activeObjects).create(eq(RepositoryPullRequestIssueKeyMapping.class), matchMapHasIssueKey("TEST-1"));
         verify(activeObjects, times(2)).delete(org.mockito.Matchers.<RawEntity<?>[]>anyVararg());
+        // the find used by delete
+        verify(activeObjects).find(eq(RepositoryPullRequestIssueKeyMapping.class), matchQueryForPRIssueKeyWithIssueKey("TEST-4"));
+        verify(activeObjects).find(eq(RepositoryPullRequestIssueKeyMapping.class), matchQueryForPRIssueKeyWithIssueKey("TEST-5"));
+    }
+
+    private Query matchQueryForPRIssueKeyWithIssueKey(final String issueKey)
+    {
+        return argThat(allOf(
+                isSelect(),
+                withWhereOnColumns(DOMAIN, PULL_REQUEST_ID, ISSUE_KEY),
+                withWhereParamsThat(Matchers.<Object>contains(REPO_ID, PR_ID, issueKey))
+        ));
+    }
+
+    private Query matchQueryForPRIssueKey()
+    {
+        return argThat(allOf(
+                isSelect(),
+                withWhereOnColumns(DOMAIN, PULL_REQUEST_ID),
+                withWhereParamsThat(Matchers.<Object>contains(REPO_ID, PR_ID))
+        ));
+    }
+
+    private Map<String, Object> matchMapHasIssueKey(String issueKey)
+    {
+        //noinspection unchecked
+        return (Map)argThat(hasEntry(ISSUE_KEY, (Object) issueKey));
     }
 
     private Map<String, Object> matchMap()
@@ -250,6 +283,7 @@ public class RepositoryPullRequestDaoImplTest
     private RepositoryPullRequestMapping mockNewPRWithCommits(final String title, final String sourceBranch, String... issueKeys)
     {
         RepositoryPullRequestMapping mapping = mock(RepositoryPullRequestMapping.class);
+        when(mapping.getID()).thenReturn(PR_ID);
         when(mapping.getName()).thenReturn(title);
         when(mapping.getSourceBranch()).thenReturn(sourceBranch);
 
