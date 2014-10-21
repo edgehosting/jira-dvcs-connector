2188d621cdc9a8d0ff234eff906757281eaa2e22
Ben Morgan <bmorgan@atlassian.com>
2014-10-21 11:24:53 +1100

    missing file from prev commit

---
+++ jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/ScannerImport.java
@@ -0,0 +1,18 @@
+package com.atlassian.jira.plugins.dvcs;
+
+import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.sal.api.scheduling.PluginScheduler;
+
+/**
+ * This class exists to provide @ComponentImport annotations for scanning for classes that are not annotated
+ * on an actual DVCS class i.e. the pluginScheduler for the schedular compat
+ *
+ * @since v6.3
+ */
+@Scanned
+public class ScannerImport
+{
+    @ComponentImport
+    private PluginScheduler pluginScheduler;
+}
