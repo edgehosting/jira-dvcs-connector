6c02149d0566493a81cee0e74f9818d86453d74e
Ben Morgan <bmorgan@atlassian.com>
2014-11-25 15:33:39 +1100

    BBC-937 Bumped version of QDSL to use the newly released streamy fold and brought it into the ChangesetQDSL code

--- jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/dao/impl/querydsl/ChangesetQDSL.java
+++ jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/dao/impl/querydsl/ChangesetQDSL.java
@@ -54,23 +54,11 @@ public class ChangesetQDSL
         this.schemaProvider = schemaProvider;
     }
 
-    /**
-     * Returns the changesets that are associated with the supplied issue key and are in a enabled, linked repository
-     * for the specified type of dvcs NOTE that this method does not filter duplicates and you will receive one
-     * Changeset per link to an issue key. This is intentional as it will allow us to modify this in future so that we
-     * match changeset information with issue key information. Also note that it does not limit the number of issue
-     * keys, if you supply more than the database can handle with an in it will fail.
-     * <p/>
-     * Recommended usage is to break up the keys prior to calling and de-duplicate on the outside as this provides us
-     * with a path to replace with a call that does the issue key grouping (probably involves changing the return
-     * signature as well).
-     */
     public List<Changeset> getByIssueKey(final Iterable<String> issueKeys, @Nullable final String dvcsType,
             final boolean newestFirst)
     {
         ByIssueKeyClosure closure = new ByIssueKeyClosure(dvcsType, issueKeys, schemaProvider);
-        StreamyResult streamyResult = queryFactory.select(closure.query());
-        Map<Integer, Changeset> changesetsById = streamyResult.foldLeft(closure.getInitialValue(), closure.getFoldFunction());
+        Map<Integer, Changeset> changesetsById = queryFactory.streamyFold(new HashMap<Integer, Changeset>(), closure);
 
         final ArrayList<Changeset> result = new ArrayList<Changeset>(changesetsById.values());
         Collections.sort(result, new ChangesetDateComparator(newestFirst));
@@ -78,7 +66,7 @@ public class ChangesetQDSL
     }
 
     @VisibleForTesting
-    static class ByIssueKeyClosure
+    static class ByIssueKeyClosure implements QueryFactory.StreamyFoldClosure<Map<Integer, Changeset>>
     {
         final String dvcsType;
         final Iterable<String> issueKeys;
@@ -100,7 +88,8 @@ public class ChangesetQDSL
             this.orgMapping = QOrganizationMapping.withSchema(schemaProvider);
         }
 
-        Function<SelectQuery, StreamyResult> query()
+        @Override
+        public Function<SelectQuery, StreamyResult> query()
         {
             return new Function<SelectQuery, StreamyResult>()
             {
@@ -137,12 +126,8 @@ public class ChangesetQDSL
             };
         }
 
-        Map<Integer, Changeset> getInitialValue()
-        {
-            return new HashMap<Integer, Changeset>();
-        }
-
-        Function2<Map<Integer, Changeset>, Tuple, Map<Integer, Changeset>> getFoldFunction()
+        @Override
+        public Function2<Map<Integer, Changeset>, Tuple, Map<Integer, Changeset>> getFoldFunction()
         {
             return new Function2<Map<Integer, Changeset>, Tuple, Map<Integer, Changeset>>()
             {
