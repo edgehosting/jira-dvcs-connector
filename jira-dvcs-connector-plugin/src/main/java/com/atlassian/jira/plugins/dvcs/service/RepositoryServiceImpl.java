09840d2fa63b1b3f6b1b1feeb691f9703a8f2211
Ben Morgan <bmorgan@atlassian.com>
2015-03-13 13:34:29 +1100

    BBC-971 These changes make DVCS connector start with newer versions of JIRA. Without these changes you will get a null pointer in the repository DAO as spring does not wire it properly.
    Technically the exclusion on bridge method is not required but keeps us consistent with JIRA.
    Also the import - export package directive changes for felix in the pom are not required but they mean that we are better behaved with our imports.

--- jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/service/RepositoryServiceImpl.java
+++ jira-dvcs-connector-plugin/src/main/java/com/atlassian/jira/plugins/dvcs/service/RepositoryServiceImpl.java
@@ -60,7 +60,7 @@ public class RepositoryServiceImpl implements RepositoryService
     @Resource
     private DvcsCommunicatorProvider communicatorProvider;
 
-    @Resource
+    // This one is wired in via a setter injection because otherwise we get a null bean for some reason
     private RepositoryDao repositoryDao;
 
     @Resource
@@ -790,5 +790,11 @@ public class RepositoryServiceImpl implements RepositoryService
     private UnknownUser getUnknownUser(Repository repository, String username, String rawUser)
     {
         return new UnknownUser(username, rawUser != null ? rawUser : username, repository.getOrgHostUrl());
+    }
+
+    @Resource
+    private void setRepositoryDao(RepositoryDao repositoryDao)
+    {
+        this.repositoryDao = repositoryDao;
     }
 }
