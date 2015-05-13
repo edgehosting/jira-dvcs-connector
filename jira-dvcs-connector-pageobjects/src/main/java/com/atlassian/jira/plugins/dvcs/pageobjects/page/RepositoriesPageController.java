eceb01588d84092d2853577e85d46bee73d38c0a
Ahmed Fouad <afouad@atlassian.com>
2015-05-13 15:06:12 +1000

    increase sync timeout to 3 minutes add logging message

--- jira-dvcs-connector-pageobjects/src/main/java/com/atlassian/jira/plugins/dvcs/pageobjects/page/RepositoriesPageController.java
+++ jira-dvcs-connector-pageobjects/src/main/java/com/atlassian/jira/plugins/dvcs/pageobjects/page/RepositoriesPageController.java
@@ -1,5 +1,7 @@
 package com.atlassian.jira.plugins.dvcs.pageobjects.page;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.atlassian.jira.pageobjects.JiraTestedProduct;
 import com.atlassian.jira.plugins.dvcs.model.Repository;
 import com.atlassian.jira.plugins.dvcs.model.RepositoryList;
@@ -20,7 +22,8 @@ public class RepositoriesPageController implements PageController<RepositoriesPa
 
     private final JiraTestedProduct jira;
     private final RepositoriesPage page;
-    private final long MAX_WAITING_TIME = 60*1000*2; // 120 seconds
+    private final long MAX_WAITING_TIME = 60*1000*3; // 180 seconds
+    private final Logger log = LoggerFactory.getLogger(RepositoriesPageController.class);
 
     public RepositoriesPageController(JiraTestedProduct jira)
     {
@@ -116,6 +119,7 @@ public class RepositoriesPageController implements PageController<RepositoriesPa
             catch (InterruptedException e)
             {
                 // ignore
+                log.debug("Failed to complete sync in " + MAX_WAITING_TIME + " milliseconds");
             }
         }
         while (!isSyncFinished());
