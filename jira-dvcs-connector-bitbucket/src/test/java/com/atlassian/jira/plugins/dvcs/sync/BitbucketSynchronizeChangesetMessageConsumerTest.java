6bc6da6af38ea4fef8a6bf7e09bb4f55456ad3b2
Hamish Farrant <hfarrant@atlassian.com>
2015-05-20 23:19:35 +0000

    FUSE-2468 removed some redundant whitespace

--- jira-dvcs-connector-bitbucket/src/test/java/com/atlassian/jira/plugins/dvcs/sync/BitbucketSynchronizeChangesetMessageConsumerTest.java
+++ jira-dvcs-connector-bitbucket/src/test/java/com/atlassian/jira/plugins/dvcs/sync/BitbucketSynchronizeChangesetMessageConsumerTest.java
@@ -114,9 +114,7 @@ public class BitbucketSynchronizeChangesetMessageConsumerTest
         when(messagingService.get(eq(BitbucketSynchronizeChangesetMessage.class), anyString())).thenReturn(messageAddress);
         when(communicator.getNextPage(eq(repository),
                 eq(includeNodes), eq(excludeNodes), any(BitbucketChangesetPage.class))).thenReturn(lastChangesetPage);
-
         when(repositoryService.get(repoId)).thenReturn(repository);
-
     }
 
     @Test
